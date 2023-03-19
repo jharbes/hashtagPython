c5c0e5a754eff3fa8cb79e124e1d4cad826478a9
Jorge Harbes <jharbes@hotmail.com>
2023-03-19 17:35:07 -0300

    aula 03 acessando com selenium

---
+++ 025-automacaoWeb-selenium/03-acessarPaginaComSelenium/aula03-acessarPaginaSelenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
+
+time.sleep(5)
