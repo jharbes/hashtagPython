2e44ba71a41ad2775b2b5c790fde2534703b4f95
Jorge Harbes <jharbes@hotmail.com>
2023-03-19 17:44:47 -0300

    adicionando links

--- 025-automacaoWeb-selenium/03-acessarPaginaComSelenium/aula03-acessarPaginaSelenium.py
+++ 025-automacaoWeb-selenium/03-acessarPaginaComSelenium/aula03-acessarPaginaSelenium.py
@@ -7,4 +7,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
+# navegador.get(r'C:\Users\Jorge\Desktop\hashtag\hashtagPython\025-automacaoWeb-selenium\03-acessarPaginaComSelenium\teste.html')
+
+navegador.get(r'C:\Users\Jorge\Desktop\hashtag\hashtagPython\025-automacaoWeb-selenium\03-acessarPaginaComSelenium\Pagina Hashtag.html')
+
 time.sleep(5)
