25a21cffbc68d35af3943a81b6138b10626d914a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-20 10:50:21 -0300

    Create automacaoErp.py

---
+++ 047-rpaComPython-AutomatizacaoProcessosSistemas/automacaoErp.py
@@ -0,0 +1,10 @@
+"""
+Existem duas maneiras de automatizar a integração com ERPs:
+
+1a- Banco de dados (geralmente python com SQL, acesso improvavel de ser conseguido)
+
+2a- Automacao via desktop (simular um usuario usando o PC para automatizar o processo, geralmente com selenium e/ou pyautogui)
+
+ERP -> Sistema de gerenciamento da empresa, a maioria nao eh via web, nao acessivel por selenium
+"""
+
