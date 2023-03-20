4e11177e18b0025473d81820583c9f8225f5331e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-20 11:39:39 -0300

    Update automacaoErp.py

--- 047-rpaComPython-AutomatizacaoProcessosSistemas/03-automacaoDeErps/automacaoErp.py
+++ 047-rpaComPython-AutomatizacaoProcessosSistemas/03-automacaoDeErps/automacaoErp.py
@@ -6,7 +6,33 @@ Existem duas maneiras de automatizar a integração com ERPs:
 2a- Automacao via desktop (simular um usuario usando o PC para automatizar o processo, geralmente com selenium e/ou pyautogui)
 
 ERP -> Sistema de gerenciamento da empresa, a maioria nao eh via web, nao acessivel por selenium (Totvs, SAP)
+
+
+Nesse projeto utilizaremos reconhecimentos de imagens, para isso precisaremos das bibliotecas pyautogui, Pillow e opencv-python
 """
 
 # fakturama -> link na descrição -> ERP de exemplo que vamos usar
-# pyautogui -> é a ferramenta que a gente vai usar pra automatizar ERP
+# pyautogui -> é a ferramenta que a gente vai usar pra automatizar ERP 
+
+"""
+Passo a passo do processo:
+
+1- Abrir o ERP (Fakturama)
+
+2- Clicar no menu new
+
+3- Clicar em new product
+
+4- Preencher todos os campos
+
+5- Selecionar a imagem
+
+6- Clicar em salvar
+"""
+
+import pyautogui
+import subprocess # utilizado para rodar um processo no computador, usaremos para abrir o ERP
+
+
+# 1- Abrir o ERP (Fakturama)
+subprocess.Popen([r"C:\Program Files\Fakturama2\Fakturama.exe"]) # acertar o diretorio
