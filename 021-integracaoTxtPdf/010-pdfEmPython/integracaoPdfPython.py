bc06260fb590dcb0cff61c6f2942ecedcdce196c
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-21 16:39:56 -0300

    paginas criadas pdf

--- 021-integracaoTxtPdf/010-pdfEmPython/integracaoPdfPython.py
+++ 021-integracaoTxtPdf/010-pdfEmPython/integracaoPdfPython.py
@@ -20,7 +20,7 @@ pyf.PdfFileReader # para ler um arquivo em PDF
 pyf.PdfFileWriter # criar/escrever um arquivo em PDF
 pyf.PdfFileMerger # mesclar arquivos PDF
 
-
+pastaAtualTrabalho=r'C:\Users\jharbes\Documents\GitHub\hashtagPython\021-integracaoTxtPdf\010-pdfEmPython'
 
 # #### 1º Objetivo: Queremos conseguir separar apenas o DRE do Release de Resultados (Página 14) para enviar para a Diretoria, como fazemos?
 
@@ -31,14 +31,15 @@ arquivoPdf=pyf.PdfReader(nomeArquivoPdf)
 print(arquivoPdf)
 
 # torna o arquivo iteravel, o for vai rolar pagina por pagina
+numPagina=1
 for pagina in arquivoPdf.pages: 
     # cria o arquivo com uma pagina apenas
     arquivoNovo=pyf.PdfWriter()
-    arquivoNovo.addPage(pagina)
+    arquivoNovo.add_page(pagina)
     # salva o arquivo
     # abre o arquivo Arquivo1.pdf em paginas\\ no mode='wb' (modo writable)
     # escreve/salva o arquivo novo no arquivo final
-    with Path('paginas\\Arquivo1.pdf').open(mode='wb') as arquivoFinal:
+    with Path(f'paginas\\Arquivo{numPagina}.pdf').open(mode='wb') as arquivoFinal:
         arquivoNovo.write(arquivoFinal)
-
+        numPagina+=1
 
