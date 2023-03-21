47394a8c0943b269502453256599486c32ad0e48
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-21 08:27:04 -0300

    Create integracaoPdfPython.py

---
+++ 021-integracaoTxtPdf/010-pdfEmPython/integracaoPdfPython.py
@@ -0,0 +1,12 @@
+"""
+# Vamos aprender a trabalhar com PDF usando o Python
+
+- Regra geral: PDF foi feito justamente para bloquear muita coisa, então não é fácil "brincar" com um pdf
+- Mesmo assim, Python tem várias bibliotecas que vão nos ajudar, vamos focar em 2:
+    - PyPDF2
+    - Tabula
+- Ler e extrair informações de um PDF a gente consegue fazer.
+- Escrever e Editar, aí já é outra história
+
+### Para os nossos exemplos, vamos avaliar o Release de Resultados do 3º e 4º Trimestre de 2020 da Magazine Luiza
+"""
