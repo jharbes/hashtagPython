92ea596b95167ed3e2e5b612bb35f563e605c169
Jorge Harbes <jharbes@hotmail.com>
2023-03-25 15:35:31 -0300

    Create selecionandoVariosElementosDeUmaVez.py

---
+++ 025-automacaoWeb-selenium/08-selecionandoVariosElementosDeUmaVez/selecionandoVariosElementosDeUmaVez.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+import time
+
+navegador=webdriver.Chrome()
+
+import os
+import time
+
+caminho = os.getcwd()
+arquivo = caminho + r"\Pagina Hashtag.html"
+navegador.get(arquivo)
+
+"""
+MANEIRAS DE SELECIONAR NO SELENIUM:
+
+id                      .ID
+xpath                   .XPATH
+class_name              .CLASS_NAME
+texto inteiro de link   .LINK_TEXT
+pedaço de texto de link .PARTIAL_LINK_TEXT
+name                    .NAME
+tag name                .TAG_NAME
+css selector            .CSS_SELECTOR
+
+Preferencia de uso:
+id
+class
+xpath
+"""
+
+
+from selenium.webdriver.common.by import By
+
+
+# #### Selecionar vários elementos de uma vez
+
+# - Vamos selecionar pelo find_elements
+# - Vamos clicar no item Blog do menu
+
+
+
+
+
+
+time.sleep(5)
