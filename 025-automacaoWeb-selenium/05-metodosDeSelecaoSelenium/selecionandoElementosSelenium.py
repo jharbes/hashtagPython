1d591083a29a995acfcaa457d33de76aec36ede7
Jorge Harbes <jharbes@hotmail.com>
2023-03-25 11:58:10 -0300

    Create selecionandoElementosSelenium.py

---
+++ 025-automacaoWeb-selenium/05-metodosDeSelecaoSelenium/selecionandoElementosSelenium.py
@@ -0,0 +1,8 @@
+# SELECIONANDO ELEMENTOS DA PAGINA COM SELENIUM
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
