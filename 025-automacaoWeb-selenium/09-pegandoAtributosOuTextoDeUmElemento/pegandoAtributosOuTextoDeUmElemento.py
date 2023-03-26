fa5cb835820ca4e72e2b6e82c9f6c5d3906acc77
Jorge Harbes <jharbes@hotmail.com>
2023-03-26 15:24:36 -0300

    pegando atributos ou texto fim

--- 025-automacaoWeb-selenium/09-pegandoAtributosOuTextoDeUmElemento/pegandoAtributosOuTextoDeUmElemento.py
+++ 025-automacaoWeb-selenium/09-pegandoAtributosOuTextoDeUmElemento/pegandoAtributosOuTextoDeUmElemento.py
@@ -30,3 +30,28 @@ xpath
 
 
 from selenium.webdriver.common.by import By
+
+# #### Pegar informações de um elemento
+
+# - texto
+# - atributos
+
+# 
+##### Ex1: Link do Whatsapp
+# pega o atributo href do elemento, que é o link no caso
+href = navegador.find_element(By.XPATH, '/html/body/footer/div/div[1]/div[2]/div/a[2]').get_attribute('href')
+print(href)
+
+
+
+##### Ex2: Imagens dos cursos
+
+linkImagem=navegador.find_element(By.XPATH, '/html/body/section[2]/div/div[4]/figure/a/img').get_attribute('src')
+print(linkImagem)
+
+# o link nao esta no elemento em questao mas sim no elemento pai
+link = navegador.find_element(By.XPATH, '/html/body/section[2]/div/div[4]/figure/a').get_attribute('href')
+print(link)
+
+
+time.sleep(5)
