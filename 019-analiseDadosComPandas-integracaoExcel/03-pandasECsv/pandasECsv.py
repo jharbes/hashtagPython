ee11b635bf6d0dec408e2b9cfaf5c031ecec22df
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-27 08:43:20 -0300

    lendo psv com pandas commit 2

--- 019-analiseDadosComPandas-integracaoExcel/03-pandasECsv/pandasECsv.py
+++ 019-analiseDadosComPandas-integracaoExcel/03-pandasECsv/pandasECsv.py
@@ -14,6 +14,22 @@ Quase sempre quando formos "ler" um arquivo csv, vamos usar o pandas. É prátic
 import pandas as pd
 
 
-
 # - Vamos ler um arquivo real, com a Base de Dados de Vendas da Empresa "Contoso"
 
+vendasDf=pd.read_csv('Contoso - Cadastro Produtos.csv')
+print(vendasDf)
+vendasDf
+
+vendasDf2=pd.read_csv('Contoso - Vendas - 2017.csv')
+print(vendasDf2)
+vendasDf2
+
+# nesse caso o arquivo está com a separacao de colunas feitas com (;) ponto e virgula, porem o padrao do csv é a separacao por (,) virgula, logo teremos que indicar esse separador para que a leitura seja feita adequadamente.
+
+vendasDf=pd.read_csv('Contoso - Cadastro Produtos.csv',sep=';')
+print(vendasDf)
+vendasDf
+
+vendasDf2=pd.read_csv('Contoso - Vendas - 2017.csv',sep=';')
+print(vendasDf2)
+vendasDf2
