8704e9c816cfb6c170f80db42a8927b1250fceb4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-27 08:19:43 -0300

    pandas e csv inicio

---
+++ 019-analiseDadosComPandas-integracaoExcel/03-pandasECsv/pandasECsv.py
@@ -0,0 +1,19 @@
+"""
+# pandas e csv
+
+## Resumo
+
+Quase sempre quando formos "ler" um arquivo csv, vamos usar o pandas. É prático e bem eficiente.
+"""
+
+## Funcionamento
+
+# Forma mais básica: (muitas vezes não usaremos a forma mais básica)
+# dataframe = pd.read_csv('arquivo_com_extensao')
+
+import pandas as pd
+
+
+
+# - Vamos ler um arquivo real, com a Base de Dados de Vendas da Empresa "Contoso"
+
