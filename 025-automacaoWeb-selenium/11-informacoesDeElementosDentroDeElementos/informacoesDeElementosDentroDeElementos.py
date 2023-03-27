593ccefecb62840ee59df3c6df70f70cafb38d47
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-27 11:16:30 -0300

    informacoes de elementos dentro de elementos fim

--- 025-automacaoWeb-selenium/11-informacoesDeElementosDentroDeElementos/informacoesDeElementosDentroDeElementos.py
+++ 025-automacaoWeb-selenium/11-informacoesDeElementosDentroDeElementos/informacoesDeElementosDentroDeElementos.py
@@ -38,10 +38,18 @@ listaElementos=navegador.find_elements(By.TAG_NAME,'figure') # lista de figures
 print(listaElementos)
 print(len(listaElementos))
 
+
 # buscaremos os links de cada imagem dentro da tag <a> que esta dentro da tag <figure>, atributo href
+# usaremos o try para caso ele nao encontre algum link dentro de <figure> ele nao de erro e quebre o programa
 for elemento in listaElementos:
-    link=elemento.find_element(By.TAG_NAME,'a').get_attribute('href')
-    print(link)
+    try: 
+        link=elemento.find_element(By.TAG_NAME,'a').get_attribute('href')
+        print(link)
+    except:
+        continue
+
+
+# como alternativa ao try tambem podemos pegar uma lista com find_elements em vez de find_element, sendo assim depois percorremos a lista com um if impedindo de acessar os elementos vazios da lista
 
 
 time.sleep(5)
