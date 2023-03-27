bd42128435cea921a30e715a5abc18cdb0a3ba6f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-27 11:19:29 -0300

    Create preenchendoFormulariosSelenium.py

---
+++ 025-automacaoWeb-selenium/12-preenchendoFormulariosSelenium/preenchendoFormulariosSelenium.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+
+navegador=webdriver.Chrome()
+
+import os
+import time
+
+caminho = os.getcwd()
+arquivo = caminho + r"\formulario.html"
+navegador.get(arquivo)
+
+"""
+MANEIRAS DE SELECIONAR NO SELENIUM:
+
+id                      .ID
+xpath                   .XPATH
+class_name              .CLASS_NAME
+texto inteiro de link   .LINK_TEXT
+pedaço de texto de link .PARTIAL_LINK_TEXT
+name                    .NAME
+tag name                .TAG_NAME
+css selector            .CSS_SELECTOR
+
+Preferencia de uso:
+id
+class
+xpath
+"""
+
+
+from selenium.webdriver.common.by import By
+
+
+# #### Botão Padrão (clicar em botão)
+
+
+
+
+
+
+time.sleep(5)
