e6e0c84629fda3657bee21d01aca3ddddae8049a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-27 08:49:10 -0300

    pandas e csv fim

--- 019-analiseDadosComPandas-integracaoExcel/03-pandasECsv/pandasECsv.py
+++ 019-analiseDadosComPandas-integracaoExcel/03-pandasECsv/pandasECsv.py
@@ -4,6 +4,9 @@
 ## Resumo
 
 Quase sempre quando formos "ler" um arquivo csv, vamos usar o pandas. É prático e bem eficiente.
+
+DATAFRAME:
+Objeto do pandas que se constitui por uma tabela, onde podemos manipular os dados com a biblioteca pandas
 """
 
 ## Funcionamento
@@ -18,18 +21,18 @@ import pandas as pd
 
 vendasDf=pd.read_csv('Contoso - Cadastro Produtos.csv')
 print(vendasDf)
-vendasDf
+vendasDf # tambem exibe o arquivo csv
 
 vendasDf2=pd.read_csv('Contoso - Vendas - 2017.csv')
 print(vendasDf2)
-vendasDf2
+vendasDf2 # tambem exibe o arquivo csv
 
 # nesse caso o arquivo está com a separacao de colunas feitas com (;) ponto e virgula, porem o padrao do csv é a separacao por (,) virgula, logo teremos que indicar esse separador para que a leitura seja feita adequadamente.
 
 vendasDf=pd.read_csv('Contoso - Cadastro Produtos.csv',sep=';')
 print(vendasDf)
-vendasDf
+vendasDf # tambem exibe o arquivo csv
 
 vendasDf2=pd.read_csv('Contoso - Vendas - 2017.csv',sep=';')
 print(vendasDf2)
-vendasDf2
+vendasDf2 # tambem exibe o arquivo csv
