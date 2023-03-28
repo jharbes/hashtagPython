92a582e604b7d89b7ce4df630f175761ecebeda8
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-28 11:33:43 -0300

    analise e visualizacao dados pandas parte 1 fim

--- 019-analiseDadosComPandas-integracaoExcel/06-analiseVisualizacaoDadosPandas-parte1/analiseVisualizacaoDadosPandas-parte1.py
+++ 019-analiseDadosComPandas-integracaoExcel/06-analiseVisualizacaoDadosPandas-parte1/analiseVisualizacaoDadosPandas-parte1.py
@@ -17,9 +17,10 @@ Se quiser personalizar mais do que o padrão do pandas, importe o matplotlib e u
 # - Preparando as bases de dados (o que fizemos na última aula)
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # importando os arquivos
-vendasDf = pd.read_csv(r'Contoso - Vendas  - 2017.csv', sep=';')
+vendasDf = pd.read_csv(r'Contoso - Vendas - 2017.csv', sep=';')
 produtosDf = pd.read_csv(r'Contoso - Cadastro Produtos.csv', sep=';')
 lojasDf = pd.read_csv(r'Contoso - Lojas.csv', sep=';')
 clientesDf = pd.read_csv(r'Contoso - Clientes.csv', sep=';')
@@ -34,3 +35,23 @@ vendasDf = vendasDf.merge(produtosDf, on='ID Produto')
 vendasDf = vendasDf.merge(lojasDf, on='ID Loja')
 vendasDf = vendasDf.merge(clientesDf, on='ID Cliente').rename(columns={'E-mail': 'E-mail do Cliente'})
 print(vendasDf)
+
+
+
+
+### Qual cliente que comprou mais vezes?
+
+# - Usaremos o método .value_counts() para contar quantas vezes cada valor do Dataframe aparece
+# - Usaremos o método .plot() para exibir um gráfico
+
+frequenciaClientes=vendasDf['E-mail do Cliente'].value_counts()
+print(frequenciaClientes)
+# frequenciaClientes.plot() # grafico completo
+
+# frequenciaClientes[:5].plot() # aqui escolhemos os cinco primeiros clientes da tabela
+
+# frequenciaClientes[:5].plot(figsize=(15,5)) # escolhemos com figsize o tamanho do grafico a ser mostrado
+
+frequenciaClientes[:5].plot(figsize=(15,5),yticks=range(0,100,5)) # aqui colocamos o eixo y para mostrar valores de 0 a 100 de 5 em 5, alem das outras marcacoes ja feitas
+
+plt.show() # mostra o grafico 
