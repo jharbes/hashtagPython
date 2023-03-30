5989006c868099d96877085a74268fdda643ce30
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-30 11:47:43 -0300

    adicionando e modificando colunas e valores commit final

--- 019-analiseDadosComPandas-integracaoExcel/09-adicionandoEModificandoColunasDataframe/adicionandoEModificandoColunasEValores.py
+++ 019-analiseDadosComPandas-integracaoExcel/09-adicionandoEModificandoColunasDataframe/adicionandoEModificandoColunasEValores.py
@@ -5,6 +5,8 @@
 ### Vamos pegar o nosso dataframe novamente
 
 import pandas as pd
+import numpy as np
+
 
 # importando os arquivos
 vendas_df = pd.read_csv(r'Contoso - Vendas - 2017.csv', sep=';')
@@ -21,5 +23,24 @@ lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
 vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
 vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
 vendas_df = vendas_df.merge(clientes_df, on='ID Cliente').rename(columns={'E-mail': 'E-mail do Cliente'})
+
+print(vendas_df)
+vendas_df.info()
+
+
+
+
+### Agora, e se quisermos acrescentar uma coluna com o mês, o dia e o ano de cada venda (e não só a data completa)
+
+# transformando a data da venda no formato object (texto) para o formato datetime, com o formato mostramos o que cada elemento é e separamos de acordo com as barras que ja estao nas informacoes do dataframe (poderia ser diferente)
+vendas_df['Data da Venda']=pd.to_datetime(vendas_df['Data da Venda'], format='%d/%m/%Y')
+
+print(vendas_df)
+vendas_df.info()
+
+# Conforme os dicionarios em python, caso uma coluna nao exista ele cria a coluna
+vendas_df['Ano da Venda']=vendas_df['Data da Venda'].dt.year
+vendas_df['Mes da Venda']=vendas_df['Data da Venda'].dt.month
+vendas_df['Dia da Venda']=vendas_df['Data da Venda'].dt.day
 
 print(vendas_df)
