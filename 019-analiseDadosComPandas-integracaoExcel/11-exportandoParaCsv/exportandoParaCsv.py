7cea742a59ceaf8a754be26fdbfffad028d48465
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-31 09:08:13 -0300

    exportando para csv commit 2

--- 019-analiseDadosComPandas-integracaoExcel/11-exportandoParaCsv/exportandoParaCsv.py
+++ 019-analiseDadosComPandas-integracaoExcel/11-exportandoParaCsv/exportandoParaCsv.py
@@ -34,3 +34,18 @@ vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
 vendas_df = vendas_df.merge(clientes_df, on='ID Cliente').rename(columns={'E-mail': 'E-mail do Cliente'})
 
 print(vendas_df)
+
+
+
+
+# agora vamos criar o csv
+
+vendas_df.to_csv('Novo Vendas 2017.csv',sep=';')
+
+
+
+### Criando um dicionário, transformando o dicionário em um DataFrame e Exportando para csv
+
+vendas_produtos = {'iphone': [558147, 951642], 'galaxy': [712350, 244295], 'ipad': [573823, 26964], 'tv': [405252, 787604], 'máquina de café': [718654, 867660], 'kindle': [531580, 78830], 'geladeira': [973139, 710331], 'adega': [892292, 646016], 'notebook dell': [422760, 694913], 'notebook hp': [154753, 539704], 'notebook asus': [887061, 324831], 'microsoft surface': [438508, 667179], 'webcam': [237467, 295633], 'caixa de som': [489705, 725316], 'microfone': [328311, 644622], 'câmera canon': [591120, 994303]}
+
+
