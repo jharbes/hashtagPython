4ab111c1bfa35de461feaee161e3cfe665486481
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 09:31:17 -0300

    action chains commit 2

--- 025-automacaoWeb-selenium/22-actionchains-dropdownEspeciais/actionChains-dropdownEspeciais.py
+++ 025-automacaoWeb-selenium/22-actionchains-dropdownEspeciais/actionChains-dropdownEspeciais.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 navegador=webdriver.Chrome()
@@ -14,3 +15,13 @@ navegador.get(arquivo)
 
 #### Dropdown diferente em um site real
 
+
+# faremos um caminho alternativo ao mero clique no link, faremos a captura do link por meio do get atribute e depois utilizaremos no comando navegador.get(linkSql)
+
+linkSql=navegador.find_element(By.XPATH,'//*[@id="menu-item-17042"]/a').get_attribute('href')
+print(linkSql)
+
+navegador.get(linkSql)
+
+
+time.sleep(5)
