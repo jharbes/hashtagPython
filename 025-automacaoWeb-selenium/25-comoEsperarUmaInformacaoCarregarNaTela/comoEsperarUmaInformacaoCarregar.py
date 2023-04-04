130d7efb36192296cb2e5f8bcdd8b2f5ba15b05a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 13:44:35 -0300

    como esperar informacao carregar commit final

--- 025-automacaoWeb-selenium/25-comoEsperarUmaInformacaoCarregarNaTela/comoEsperarUmaInformacaoCarregar.py
+++ 025-automacaoWeb-selenium/25-comoEsperarUmaInformacaoCarregarNaTela/comoEsperarUmaInformacaoCarregar.py
@@ -9,13 +9,40 @@ navegador=webdriver.Chrome()
 import time
 
 navegador.get("https://www.hashtagtreinamentos.com/")
-time.sleep(5)
+time.sleep(15)
+navegador.find_element(By.CLASS_NAME, 'eicon-close').click()
+
+time.sleep(2)
+
+
+
+
+# loop
+# fica tentando encontrar o elemento que possui aquela class name dentro do loop, quando a lista tiver pelo menos um elemento significa que o pop up apareceu e ele clica
+navegador.get("https://www.hashtagtreinamentos.com/")
+
+while len(navegador.find_elements(By.CLASS_NAME, 'eicon-close')) < 1:
+    time.sleep(1)
+time.sleep(1) # garantia
+
 navegador.find_element(By.CLASS_NAME, 'eicon-close').click()
 
 
 
+time.sleep(2)
 
+# EC WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+navegador.get("https://www.hashtagtreinamentos.com/")
+
+# nesse caso ele vai esperar pelo aparecimento do elemento por até 20 segundos, quando aparecer ele vai seguir com o codigo
+# a informacao passada do elemento sera uma tupla, por isso possui dois parenteses
+elemento = WebDriverWait(navegador, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'eicon-close')))
+time.sleep(1) # garantia
+
+elemento.click()
 
 
 time.sleep(5)
