61993600774124b46dcd77e1f29862746911f264
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 08:30:28 -0300

    action chains, dropdown especiais inicio

---
+++ 025-automacaoWeb-selenium/22-actionchains-dropdownEspeciais/actionChains-dropdownEspeciais.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+
+navegador=webdriver.Chrome()
+
+import os
+import time
+
+caminho = os.getcwd()
+arquivo = caminho + r"\Pagina Hashtag.html"
+navegador.get(arquivo)
+
+
+
+#### Dropdown diferente em um site real
+
