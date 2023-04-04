dbe0b17d98c201a2d1f7f5e726f2109a098609e4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 11:27:30 -0300

    diferentes abas e janelas commit 2

--- 025-automacaoWeb-selenium/24-trabalhandoComDiferentesAbasEJanelas/trabalhandoComDiferentesAbasEJanelas.py
+++ 025-automacaoWeb-selenium/24-trabalhandoComDiferentesAbasEJanelas/trabalhandoComDiferentesAbasEJanelas.py
@@ -9,12 +9,25 @@ caminho = os.getcwd()
 arquivo = caminho + r"\Pagina Hashtag.html"
 navegador.get(arquivo)
 
+"""
+
+Por padrão o selenium não troca de aba automaticamente quando ao clicar em algum link uma nova aba ou janela é aberta, para isso teremos que fazer a intervencao por meio de codigo.
+
+"""
 
 #### Botão que abre outra janela
 
+navegador.find_element(By.XPATH,'/html/body/section[2]/div/div[6]/figure/a').click()
+
+# tentando preencher o formulario
+navegador.find_element(By.NAME,'firstname').send_keys('Jorge')
 
 
 
+### Outra aba
+
+abaOriginal=
+novaAba=
 
 
 
