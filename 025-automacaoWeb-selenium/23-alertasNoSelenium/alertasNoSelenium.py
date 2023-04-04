b081e389d8f2d2735a903170f5e35742ef2fe141
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-04 10:27:12 -0300

    alertas no selenium commit 2

--- 025-automacaoWeb-selenium/23-alertasNoSelenium/alertasNoSelenium.py
+++ 025-automacaoWeb-selenium/23-alertasNoSelenium/alertasNoSelenium.py
@@ -11,6 +11,42 @@ caminho = os.getcwd()
 arquivo = caminho + r"\alertas.html"
 navegador.get(arquivo)
 
+"""
+
+Alertas são diferentes de POP Ups, os alertas não possuem elementos pois não fazem parte do site e sim do navegador, os POP Ups possuem elementos pois não fazem parte do navegador e sim do site
+
+"""
 
 #### Alertas Básicos
 
+# selecionar um alerta
+navegador.find_element(By.XPATH,'/html/body/div[1]/input').click()
+
+time.sleep(0.5)
+# forma simples de mudar para um alerta para interagir com ele
+alerta=navegador.switch_to.alert
+alerta.accept()
+
+# forma "completa" de mudar para um alerta para interagir com ele
+time.sleep(0.5)
+
+from selenium.webdriver.common.alert import Alert
+
+navegador.find_element(By.XPATH,'/html/body/div[1]/input').click()
+
+time.sleep(0.5)
+
+alerta=Alert(navegador)
+alerta.accept()
+
+
+
+
+#### Alertas de Confirmação
+
+
+
+
+
+
+time.sleep(5)
