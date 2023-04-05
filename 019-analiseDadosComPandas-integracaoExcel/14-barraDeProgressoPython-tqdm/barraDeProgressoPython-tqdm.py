27b76a21f24b9edb2bcd40a9fb070aaa85bba7eb
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-05 09:18:32 -0300

    barra de progresso python commit inicial

---
+++ 019-analiseDadosComPandas-integracaoExcel/14-barraDeProgressoPython-tqdm/barraDeProgressoPython-tqdm.py
@@ -0,0 +1,36 @@
+"""
+
+# E quando o nosso código demora muito? Será que travou? Quanto tempo vai demorar?
+
+### Biblioteca/Módulo tqdm
+
+- Vamos importar os arquivos csv da Empresa Contoso e tratar como fizemos ao longo desse módulo
+
+"""
+
+import pandas as pd
+
+# importando os arquivos
+vendas_df = pd.read_csv(r'Contoso - Vendas - 2017.csv', sep=';')
+produtos_df = pd.read_csv(r'Contoso - Cadastro Produtos.csv', sep=';')
+lojas_df = pd.read_csv(r'Contoso - Lojas.csv', sep=';')
+clientes_df = pd.read_csv(r'Contoso - Clientes.csv', sep=';')
+
+# limpando apenas as colunas que queremos
+clientes_df = clientes_df[['ID Cliente', 'E-mail']]
+produtos_df = produtos_df[['ID Produto', 'Nome do Produto']]
+lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
+
+# mesclando e renomeando os dataframes
+vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
+vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
+vendas_df = vendas_df.merge(clientes_df, on='ID Cliente').rename(columns={'E-mail': 'E-mail do Cliente'})
+print(vendas_df)
+
+
+
+
+### Agora, imagina que a Loja Contoso Roma (ID 222), para tentar burlar o sistema de metas, diminuiu 1 da quantidade devolvida de todas as vendas que ela teve. Descobrindo isso, precisamos ajeitar a base
+
+# - Farei com um for, principalmente por motivos didáticos, mas teríamos outras formas de fazer isso também.
+
