50bbb04043bf99b92c458009dc395d26d4a98170
Jorge Harbes <jharbes@hotmail.com>
2023-04-07 18:53:43 -0300

    printscreen com selenium fim

--- 025-automacaoWeb-selenium/28-printscreenComSelenium/printscreenComSelenium.py
+++ 025-automacaoWeb-selenium/28-printscreenComSelenium/printscreenComSelenium.py
@@ -12,3 +12,28 @@ navegador.get(arquivo)
 
 ### Tirando print da tela inteira
 
+navegador.save_screenshot('print-telaInteira.png')
+
+
+
+
+### Tirando print de parte da tela
+
+from PIL import Image
+
+imagem=Image.open('print-telaInteira.png')
+
+elemento=navegador.find_element(By.ID,'header')
+posicao=elemento.location
+tamanho=elemento.size
+print(posicao)
+print(tamanho)
+
+xInicial=posicao['x']
+yInicial=posicao['y']
+
+xFinal=posicao['x']+tamanho['width']
+yFinal=posicao['y']+tamanho['height']
+
+imagem=imagem.crop((xInicial,yInicial,xFinal,yFinal))
+imagem.save('print-pedacoTela.png')
