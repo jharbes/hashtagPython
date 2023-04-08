9a0fef54fa2b1b18c3b7834c574cc9ce6242a844
Jorge Harbes <jharbes@hotmail.com>
2023-04-08 15:25:38 -0300

    Create executandoJavaScriptComSelenium.py

---
+++ 025-automacaoWeb-selenium/33-executandoJavaScriptComSelenium/executandoJavaScriptComSelenium.py
@@ -0,0 +1,19 @@
+"""
+
+### Executar Comandos Javascript (Scroll da tela)
+
+- Você consegue, por meio do Selenium, executar comandos javascript no seu navegador
+
+- Isso é essencial para dar scroll na tela, por exemplo, caso seja necessário, como no youtube
+
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import os, time
+
+options=webdriver.ChromeOptions()
+# chrome://version
+options.add_argument(r'user-data-dir=C:\Users\Jorge\AppData\Local\Google\Chrome\User Data\Profile Selenium')
+navegador=webdriver.Chrome(options=options)
