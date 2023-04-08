d4c29306e9f664a1618c570e54c879e85354ee5a
Jorge Harbes <jharbes@hotmail.com>
2023-04-08 16:31:21 -0300

    executando javascript com selenium commit 2

--- 025-automacaoWeb-selenium/33-executandoJavaScriptComSelenium/executandoJavaScriptComSelenium.py
+++ 025-automacaoWeb-selenium/33-executandoJavaScriptComSelenium/executandoJavaScriptComSelenium.py
@@ -7,6 +7,10 @@
 - Isso é essencial para dar scroll na tela, por exemplo, caso seja necessário, como no youtube
 
 """
+# comando que dá scroll na janela em javascript é o window.scroll(x,y) onde x e y é o local para onde você quer ir na tela, exemplo:
+
+# window.scroll(0, 100) onde x seria posicao zero e y posicao 100 (geralmente a posicao x sempre sera zero pois os sites nao costumam ter scroll lateral)
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -19,8 +23,21 @@ options.add_argument(r'user-data-dir=C:\Users\Jorge\AppData\Local\Google\Chrome\
 navegador=webdriver.Chrome(options=options)
 
 
+# quero pegar uma lista de pelo menos 50 vídeos de Python do Youtube
+
 navegador.get("https://www.youtube.com/results?search_query=python")
 
+i=0
+while len(navegador.find_elements(By.ID,'thumbnail'))<1:
+    time.sleep(3)
+    i+=1
+    if i>30:
+        break
+
+listaVideos=navegador.find_elements(By.ID,'thumbnail')
+
+for video in listaVideos:
+    print(video.get_attribute('href'))
 
 
 
