99b750508ef1de86c4a338d29e7766911d8a8f69
Jorge Harbes <jharbes@hotmail.com>
2023-04-08 12:05:53 -0300

    automacao envio msg whatsapp commit 2

--- 025-automacaoWeb-selenium/32-desafio-automacaoEnvioMsgsWhatsapp/automacaoEnvioMsgsWhatsapp.py
+++ 025-automacaoWeb-selenium/32-desafio-automacaoEnvioMsgsWhatsapp/automacaoEnvioMsgsWhatsapp.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
@@ -13,6 +14,32 @@ navegador=webdriver.Chrome(options=options)
 navegador.get('https://web.whatsapp.com')
 
 import os, time
+import pandas as pd
+
+tabela=pd.read_excel('Envios.xlsx')
+print(tabela)
+
+# o whatsapp ja carregou
+
+import urllib
+
+for linha in tabela.index:
+    nome=tabela.loc[linha,'nome']
+    mensagem=tabela.loc[linha,'mensagem']
+    arquivo=tabela.loc[linha,'arquivo']
+    telefone=tabela.loc[linha,'telefone']
+
+    texto=mensagem.replace('fulano',nome) # alteramos o fulano da mensagem pelo nome da pessoa a ser enviado
+
+    texto=urllib.parse.quote(texto) # aqui formatamos o texto a ser enviado para que ele tenha o formato de link e possa ser enviado como um link
+
+    # enviar a mensagem
+    link=f'https://web.whatsapp.com/send?phone={telefone}&text={texto}'
+
+    navegador.get(link)
+
+    # esperar a tela do whatsapp carregar
+    
 
 
 
@@ -20,4 +47,4 @@ import os, time
 
 
 
-time.sleep(10)
+time.sleep(20)
