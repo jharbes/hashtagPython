c41bbd282517743da1406db9e98d9d6017467fb5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-19 13:28:52 -0300

    Update mentoria-leituraXmlENotasfiscais.py

--- 021-integracaoTxtPdf/08-mentoria-leituraXmlENotasfiscais/mentoria-leituraXmlENotasfiscais.py
+++ 021-integracaoTxtPdf/08-mentoria-leituraXmlENotasfiscais/mentoria-leituraXmlENotasfiscais.py
@@ -1,7 +1,15 @@
 import xmltodict
 
 # with abre o arquivo e ja garante que no final dela o arquivo sera fechado automaticamente
-with open('.\\021-integracaoTxtPdf\\08-mentoria-leituraXmlENotasfiscais\\NFs Finais\\DANFEBrota.xml','rb') as arquivo:
-    documento=xmltodict.parse(arquivo)
+
+try:
+    caminhoArquivoXml='.\\021-integracaoTxtPdf\\08-mentoria-leituraXmlENotasfiscais\\NFs Finais\\DANFEBrota.xml'
+    with open(caminhoArquivoXml,'rb') as arquivo:
+        documento=xmltodict.parse(arquivo)
+except:
+    caminhoArquivoXml='NFs Finais\\DANFEBrota.xml'
+    with open(caminhoArquivoXml,'rb') as arquivo:
+        documento=xmltodict.parse(arquivo)
+
 
 print(documento)
