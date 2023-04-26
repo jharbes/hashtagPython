7e8f6550ac8a388d1aebd37b328d1e29dafbbd3b
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-26 09:43:18 -0300

    Create ambientesVirtuais.py

---
+++ 030-ambientesVirtuais/ambientesVirtuais.py
@@ -0,0 +1,33 @@
+"""
+
+# Ambientes Virtuais no Python
+
+### Essencialmente usamos o virtualenv, mas o anaconda tem um método próprio para isso
+
+### Objetivo: Ter outras versões do Python instalados no computador com 2 objetivos:
+    
+    1. Usar ferramentas que funcionam apenas com versões específicas do Python
+    2. Isolar de todo o resto tudo aquilo que é estritamente necessário para o programa rodar. Importante para sites e programas que vão ser distribuídos
+    
+### Observação Importante:
+
+Quando você cria um ambiente virtual, ele vem praticamente sem nada, então você precisa usar o pip ou o conda para instalar cada pacote que for usar
+
+"""
+
+### Comandos importantes do cmd:
+
+# - Windows:
+# cd Pasta -> navega até aquela pasta
+# cd .. -> volta para a pasta anterior
+# dir -> lista os arquivos contidos na pasta
+
+# - Mac ou Linux
+# cd Pasta -> navega até aquela pasta
+# cd .. -> volta para a pasta anterior
+# ls -> lista os arquivos contidos na pasta
+
+#Obs: Caso o nome da pasta tenha espaço, talvez seja necessário substituir o espaço por \, exemplo:
+# cd Python Impressionador -> navega até a pasta "Python Impressionador"
+# talvez seja necessário fazer:
+# cd Python\Impressionador
