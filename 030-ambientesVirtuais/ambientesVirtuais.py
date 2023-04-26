85ad052f62e87424905fe9c0b567a48782040749
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-26 10:12:16 -0300

    Update ambientesVirtuais.py

--- 030-ambientesVirtuais/ambientesVirtuais.py
+++ 030-ambientesVirtuais/ambientesVirtuais.py
@@ -27,7 +27,57 @@ Quando você cria um ambiente virtual, ele vem praticamente sem nada, então voc
 # cd .. -> volta para a pasta anterior
 # ls -> lista os arquivos contidos na pasta
 
-#Obs: Caso o nome da pasta tenha espaço, talvez seja necessário substituir o espaço por \, exemplo:
+# Obs: Caso o nome da pasta tenha espaço, talvez seja necessário substituir o espaço por \, exemplo:
 # cd Python Impressionador -> navega até a pasta "Python Impressionador"
 # talvez seja necessário fazer:
 # cd Python\Impressionador
+
+
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+
+# -m executa como script
+python -m venv nome_do_ambiente_virtual
+
+
+# Abra a pasta do seu projeto no terminal
+# e digite:
+python -m venv nome_do_ambiente_virtual
+#
+
+# Ativando e desativando meu ambiente virtual
+
+Windows: .\venv\Scripts\activate
+Linux e Mac: source venv/bin/activate
+Desativar: deactivate
+#
+
+
+# Replicando ambientes
+# No início do artigo falamos sobre os ambientes virtuais trazerem a simplicidade de replicar o ambiente em outras máquinas e isso se deve a possibilidade de exportarmos um arquivo com todas bibliotecas que o nosso projeto contém. Veja como é simples:
+
+pip freeze > requirements.txt
+
+
+# Com o comando acima, será criado um arquivo com todas as bibliotecas presentes em nosso ambiente virtual. Por exemplo:
+
+flake8==3.7.9
+Flask==1.1.2
+Flask-API==2.0
+
+
+# Agora, se quisermos rodar o nosso projeto em outra máquina, não será necessário baixar as dependências uma a uma, basta fazer:
+
+pip install -r requirements.txt  
+
+# Com o comando acima, será instalado de forma automática todas as bibliotecas presentes no arquivo requirements.txt
+
+
+# E por fim, para desativar o ambiente virtual:
+
+deactivate
