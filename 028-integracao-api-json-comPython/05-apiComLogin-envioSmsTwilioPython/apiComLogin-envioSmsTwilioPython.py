fe275936351f551ef12a4d18da4f923e398150be
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-04-28 10:47:32 -0300

    Create apiComLogin-envioSmsTwilioPython.py

---
+++ 028-integracao-api-json-comPython/05-apiComLogin-envioSmsTwilioPython/apiComLogin-envioSmsTwilioPython.py
@@ -0,0 +1,10 @@
+"""
+
+Python e API com Login
+
+O 1o passo de toda API com Login é criar uma conta e pegar suas credenciais
+
+No seu código, o 1º passo é sempre estabelecer a conexão com a API, usando seu login e suas credenciais.
+
+
+"""
