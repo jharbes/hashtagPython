4e69821633a954587df6d146168fe1f5585b0bc6
Jorge Harbes <jharbes@hotmail.com>
2023-04-30 11:56:05 -0300

    alteracao nome arquivo

--- 019-analiseDadosComPandas-integracaoExcel/23-mentoriaPython-transformarCodigoEmSistema-exAnaliseDados/transformarCodigoEmSistema.py
+++ 019-analiseDadosComPandas-integracaoExcel/23-mentoriaPython-transformarCodigoEmSistema-exAnaliseDados/transformarCodigoEmSistema.py
@@ -3,13 +3,15 @@ Como eu faço pro código rodar sem ter o python, sem o computador ligado?
 
 2 opções:
 
-    - online - API - hospedar no servidor
+    - online - API - hospedar no servidor (heroku, railway)
         - computador desligado
         - horário fixo
+        - mais difícil
 
-    - local - executável
+    - local - tela/sistema + executável
         - interage com algum arquivo/programa que só existe no meu computador
             - SAP, TOTVS
             - sob demanda
+            - mais fácil
 
 """
