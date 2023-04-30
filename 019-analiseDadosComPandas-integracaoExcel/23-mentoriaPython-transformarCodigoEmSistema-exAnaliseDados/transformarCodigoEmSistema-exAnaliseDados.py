f198deacab95428f799ab420775c4e55abb23f89
Jorge Harbes <jharbes@hotmail.com>
2023-04-30 11:19:59 -0300

    Create transformarCodigoEmSistema-exAnaliseDados.py

---
+++ 019-analiseDadosComPandas-integracaoExcel/23-mentoriaPython-transformarCodigoEmSistema-exAnaliseDados/transformarCodigoEmSistema-exAnaliseDados.py
@@ -0,0 +1,15 @@
+"""
+Como eu faço pro código rodar sem ter o python, sem o computador ligado?
+
+2 opções:
+
+    - online - API - hospedar no servidor
+        - computador desligado
+        - horário fixo
+
+    - local - executável
+        - interage com algum arquivo/programa que só existe no meu computador
+            - SAP, TOTVS
+            - sob demanda
+
+"""
