5c3ad33dd2fa6656db6a23a6e9d8bfc64e37b206
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-09 11:18:57 -0300

    Update integracaoPythonWord.py

--- 021-integracaoTxtPdf/09-mentoria-integracaoPythonWord/integracaoPythonWord.py
+++ 021-integracaoTxtPdf/09-mentoria-integracaoPythonWord/integracaoPythonWord.py
@@ -29,6 +29,24 @@ paragrafo = documento.add_paragraph(texto)
 
 ### formatação
 
+# Pt vem de pontos que seria o tamanho da fonte
+# RGBColor é das cores
+# Cm é de centimetros para editar tamanho de imagem, tamanho de margem, etc
+from docx.shared import Pt, RGBColor, Cm # valores de formatação
+
+# permite criar um estilo de formatação (determina um conjunto de características para a fonte como cor, tipo de fonte, tamanho, etc)
+from docx.enum.style import WD_STYLE_TYPE
+
+paragrafo.style = documento.styles.add_style("EstiloLira", WD_STYLE_TYPE.PARAGRAPH)
+paragrafo.style.font.name = "Algerian"
+paragrafo.style.font.size = Pt(15)
+paragrafo.style.font.bold = True
+paragrafo.style.font.italic = True
+paragrafo.style.font.underline = True
+paragrafo.style.font.color.rgb = RGBColor(255, 0, 0)
+
+
+
 
 
 
