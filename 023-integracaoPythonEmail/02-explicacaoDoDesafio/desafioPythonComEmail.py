20dcd8629ee2d3af0d5dfca451c15c4955c1f84f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-10 14:25:23 -0300

    Create desafioPythonComEmail.py

---
+++ 023-integracaoPythonEmail/02-explicacaoDoDesafio/desafioPythonComEmail.py
@@ -0,0 +1,23 @@
+"""
+# Desafio Python e E-mail
+
+### Descrição
+
+Digamos que você trabalha em uma indústria e está responsável pela área de inteligência de negócio.
+
+Todo dia, você, a equipe ou até mesmo um programa, gera um report diferente para cada área da empresa:
+- Financeiro
+- Logística
+- Manutenção
+- Marketing
+- Operações
+- Produção
+- Vendas
+
+Cada um desses reports deve ser enviado por e-mail para o Gerente de cada Área.
+
+Crie um programa que faça isso automaticamente. A relação de Gerentes (com seus respectivos e-mails) e áreas está no arquivo 'Enviar E-mails.xlsx'.
+
+Dica: Use o pandas read_excel para ler o arquivo dos e-mails que isso vai facilitar.
+
+"""
