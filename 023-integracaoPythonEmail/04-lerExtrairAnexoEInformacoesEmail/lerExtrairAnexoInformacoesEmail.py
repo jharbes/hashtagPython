b34cd25cb60f16005db511995da0bf4b2216c908
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-11 12:04:36 -0300

    Update lerExtrairAnexoInformacoesEmail.py

--- 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
+++ 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
@@ -3,3 +3,9 @@ Ler e extrair anexo e informacoes do Email
 
 """
 
+from imap_tools import MailBox, AND
+
+usuario='jharbes@hotmail.com'
+senha=''
+
+meuEmail=MailBox('outlook.office365.com').login(usuario,senha)
