cddef8297ce22c02368452b6d6fe640e9e9a3617
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-16 14:53:54 -0300

    Update main.py

--- 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/main.py
+++ 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/main.py
@@ -1,5 +1,9 @@
 from contasBancos import *
 
 c1=ContaCorrente('Jorge Nami Harbes',10231086741,500)
+c2=ContaCorrente('Carolina Ferreira Alcantara',16242112752,1500)
+c3=ContaCorrente('Elizabeth Barreto Ramos Ferreira Harbes',39634299768,500000)
 
 print(c1.__dict__)
+
+print(numerosConta.NumerosConta.numerosConta)
