dc7ca4a3f998f82f5c7ea972b42975f3e1eddb8e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-16 15:20:46 -0300

    Update main.py

--- 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/main.py
+++ 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/main.py
@@ -1,4 +1,7 @@
 from contasBancos import *
+from numerosConta import *
+
+contasCorrentes=[]
 
 c1=ContaCorrente('Jorge Nami Harbes',10231086741,500)
 c2=ContaCorrente('Carolina Ferreira Alcantara',16242112752,1500)
@@ -9,6 +12,7 @@ print(c1.__dict__)
 print(numerosConta.NumerosConta.numerosConta)
 
 c1.saque(200)
+c3.deposito(10000000000000)
 
 print(c1.saldo)
 
@@ -16,4 +20,15 @@ c1.numero=55555
 
 print(c1.numero)
 
+print(vars(c3))
+
 print(numerosConta.NumerosConta.numerosConta)
+
+
+contasCorrentes.append(ContaCorrente('Jorge Nami Harbes',10231086741,500))
+
+
+print(contasCorrentes)
+
+for conta in contasCorrentes:
+    print(conta.__dict__)
