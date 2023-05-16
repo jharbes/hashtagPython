89bd37e63927154e1623c707654d96399b81c869
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-16 13:18:55 -0300

    Create oQueEhSelfDasClasses.py

---
+++ 037-programacaoOrientadaObjetos/07-oQueEhSelfDasClasses/oQueEhSelfDasClasses.py
@@ -0,0 +1,9 @@
+# Criando nossa 1ª Classe em Python
+# Sempre que você quiser criar uma classe, você vai fazer:
+#
+# class Nome_Classe:
+#
+# Dentro da classe, você vai criar a "função" (método) __init__
+# Esse método é quem define o que acontece quando você cria uma instância da Classe
+#
+# Vamos ver um exemplo para ficar mais claro, com o caso da Televisão que a gente vinha comentando
