818b8c8acf0abd0a0fec3e1860e82cb010340cd3
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-17 14:39:00 -0300

    reinclusao

---
+++ 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/ContasBancos.py
@@ -0,0 +1,75 @@
+from UI import ui
+from NumerosConta import NumerosConta
+
+
+# criando formatacao para os valores (em reais)
+import locale
+locale.setlocale(locale.LC_MONETARY,'pt_BR.UTF-8')
+
+def formatacaoMoeda(valor):
+    return locale.currency(valor,grouping=True)
+
+
+class ContaCorrente:
+
+
+    def __init__(self,agencia,nomeTitular,cpfTitular,saldo) -> None:
+        self.__banco='033'
+        self.__agencia=agencia
+        self.__numero=NumerosConta.gerarNumeroConta()
+        self.__nomeTitular=nomeTitular
+        self.__cpfTitular=cpfTitular
+        self.__saldo=saldo
+        self.__limite=0
+        ui('CONTA ABERTA COM SUCESSO COM OS SEGUINTES DADOS:\n{}'.format(self.__dict__))
+
+    @property
+    def banco(self):
+        return self.__banco
+    
+    @property
+    def agencia(self):
+        return self.__agencia
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    def consultarSaldo(self):
+        ui('O saldo da conta de número {} é de {}'.format(self.__numero,formatacaoMoeda(self.__saldo)))
+    
+    def limiteConta(self,valor):
+        self.__limite=valor
+
+    def consultarLimiteConta(self):
+        ui('O limite da conta de número {} possui o valor de {}'.format(self.__numero,formatacaoMoeda(self.__limite)))
+
+    def deposito(self,valor):
+        self.__saldo+=valor
+        self.consultarSaldo()
+
+    def saque(self,valor):
+        if self.__saldo+self.__limite>=valor:
+            self.__saldo-=valor
+            ui('Saque de {} efetivado com sucesso!'.format(formatacaoMoeda(valor)))
+            self.consultarSaldo()
+        else:
+            ui('Saque de {} não permitido! Valores de saldo/limite insuficientes!'.format(formatacaoMoeda(valor)))
+
+    @property
+    def numero(self):
+        return self.__numero
+    
+    @numero.setter
+    def numero(self,novoNumero):
+        if novoNumero not in NumerosConta.numerosConta:
+                NumerosConta.numerosConta.append(novoNumero)
+                self.__numero=novoNumero
+    
+    @property
+    def nomeTitular(self):
+        return self.__nomeTitular
+    
+    @property
+    def cpfTitular(self):
+        return self.__cpfTitular
---
+++ 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/main.py
@@ -0,0 +1,47 @@
+from ContasBancos import *
+from NumerosConta import *
+from UI import ui
+
+agenciaLocal='2290'
+
+cc=[]
+
+cc.append(ContaCorrente(agenciaLocal,'Jorge Nami Harbes',10231086741,500))
+cc.append(ContaCorrente(agenciaLocal,'Carolina Ferreira Alcantara',16242112752,1500))
+cc.append(ContaCorrente(agenciaLocal,'Elizabeth Barreto Ramos Ferreira Harbes',39634299768,500000))
+
+ui(cc[0].agencia)
+ui(vars(cc[0]))
+ui(cc[0].__dict__)
+
+ui(NumerosConta.numerosConta)
+
+cc[0].saque(200)
+cc[2].deposito(10000000000000)
+
+cc[0].limiteConta(1000)
+
+cc[0].saque(800)
+
+cc[0].saque(700)
+
+cc[0].saque(300)
+
+cc[0].consultarSaldo()
+
+
+
+cc[0].numero=55555
+
+
+
+ui(NumerosConta.numerosConta)
+
+
+cc.append(ContaCorrente(agenciaLocal,'Matheus Attilio',96564512785,800))
+
+
+ui(cc)
+
+# for conta in cc:
+#     ui(conta.__dict__)
