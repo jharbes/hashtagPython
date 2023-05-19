18df93c4f3f47ae648a62238ee67fd7f87b70710
Jorge Harbes <jharbes@hotmail.com>
2023-05-19 18:10:08 -0300

    puxando senha de arquivo externo

--- 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
+++ 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
@@ -3,10 +3,16 @@ Ler e extrair anexo e informacoes do Email
 
 """
 
+import sys
+sys.path.append('C:\\Users\\Jorge\\Desktop\\hashtag')
+print(sys.path)
+
+from passwd import password
+
 from imap_tools import MailBox, AND
 
 usuario='jharbes@hotmail.com'
-senha=''
+senha=password
 
 # TODO: testando o todo
 
