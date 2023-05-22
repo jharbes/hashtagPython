910b56a32baae0f1f94c1c93bc4cce31c8cec45a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-22 10:42:30 -0300

    Create CartaoCredito.py

---
+++ 037-programacaoOrientadaObjetos/12-aplicacao-criandoSistemaDeContaCorrente/CartaoCredito.py
@@ -0,0 +1,9 @@
+class CartaoCredito:
+
+    def __init__(self) -> None:
+        self.numero=None
+        self.nome=None
+        self.dataValidade=None
+        self.codSeguranca=None
+        self.limite=None
+        
