9f431ffe5b44d994a5d4bbf425f6fa86ccab2e68
Jorge Harbes <jharbes@hotmail.com>
2023-06-01 19:45:33 -0300

    passo a passo pyodbc parte2

--- 024-integracaoPythonSql/05-pyodbc-passoAPasso/pyodbc-passoAPasso.py
+++ 024-integracaoPythonSql/05-pyodbc-passoAPasso/pyodbc-passoAPasso.py
@@ -22,7 +22,33 @@ dados_conexao = ("Driver={Seu_Driver};"
 import pyodbc
 print(pyodbc.drivers())
 
-dadosConexao=('Driver={SQLite3 ODBC Driver};Server=localhost;Database=chinook.db')
+dadosConexao=('Driver={SQLite3 ODBC Driver};Server=localhost;Database=salarios.sqlite')
 
 conexao=pyodbc.connect(dadosConexao)
 print('Conexão bem sucedida')
+
+
+
+
+# 2. Agora, precisamos criar o que o pyodbc chama de cursor, que é basicamente quem vai executar os nossos códigos SQL
+
+cursor=conexao.cursor()
+
+
+
+
+# 3. Agora já podemos executar nossos comandos SQL.
+
+# Temos 2 opções aqui:
+
+#     1. Apenas Executar comandos SQL que vão acontecer no banco
+#     2. Executar o comando SQL e armazenar a consulta em uma variável no Python, como um dataframe do pandas
+
+# dataframe = pd.read_sql('COMANDO_SQL', conexao) -> vai executar o comando SQL, gerando uma consulta e retornando a resposta dessa consulta para o dataframe
+
+# Para pegar uma tabela inteira do nosso banco de dados, usaremos o comando SQL Select *:
+# "SELECT * FROM BaseDeDados.Tabela"
+
+
+# o comando cursor execute executa o comando sql que está em seu argumento
+cursor.execute('SELECT * FROM Salaries')
