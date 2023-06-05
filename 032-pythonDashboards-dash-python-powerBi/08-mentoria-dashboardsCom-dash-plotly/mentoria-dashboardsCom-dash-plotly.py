b097aa369c887609eabc568756ed5d8cba601014
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-05 09:43:29 -0300

    criacao dos arquivos necessarios

---
+++ 032-pythonDashboards-dash-python-powerBi/08-mentoria-dashboardsCom-dash-plotly/mentoria-dashboardsCom-dash-plotly.py
@@ -0,0 +1,23 @@
+"""
+### Dash
+
+- Instalar o dash
+
+
+Estrutura Básica
+- plotly -> gráficos
+- Flask -> aplicação
+
+
+
+### Como funciona o Dash
+
+- Layout
+    - HTML -> textos, imagens, espaços
+    - Dash Components (Core Components) -> gráficos, botões que mexem em gráficos, coisas do dashboard
+- Callbacks
+
+"""
+
+#### Primeiro Dashboard
+
