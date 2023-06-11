29e370a6813d96eb95d495c42442dd23f9bdf2b4
Jorge Harbes <jharbes@hotmail.com>
2023-06-11 15:54:02 -0300

    finalizacao adicionando insumo aula 15

--- 024-integracaoPythonSql/13-ex02-crud-controleDeEstoque/PythonESQL-ControleDeEstoque.py
+++ 024-integracaoPythonSql/13-ex02-crud-controleDeEstoque/PythonESQL-ControleDeEstoque.py
@@ -13,17 +13,26 @@ cursor=conexao.cursor()
 
 
 def adicionar_insumo():
-    print(caixa_texto.get('1.0', END))
-    print(nome_insumo.get())
-    print(data_insumo.get())
-    print(lote_insumo.get())
-    print(qtde_insumo.get())
+
+    cursor.execute(f"""
+    INSERT INTO Estoque (Produto, Quantidade, DataValidade, Lote)
+    VALUES
+    ('{nome_insumo.get()}',{qtde_insumo.get()},'{data_insumo.get()}',{lote_insumo.get()})
+    """)
+
+    cursor.commit()
+
+    # print(caixa_texto.get('1.0', END))
+    # print(nome_insumo.get())
+    # print(data_insumo.get())
+    # print(lote_insumo.get())
+    # print(qtde_insumo.get())
     
-    # deletar tudo da caixa de texto
+    # deletar tudo da caixa de texto caso ja tenha alguma coisa inserida anteriormente na caixa de texto
     caixa_texto.delete("1.0", END)
     
-    # escrever na caixa de texto
-    caixa_texto.insert("1.0", "Texto")
+    # escrever na caixa de texto uma mensagem de sucesso
+    caixa_texto.insert("1.0", f"Insumo {nome_insumo.get()} adicionado com sucesso!")
     
 def deletar_insumo():
     print("deletar_insumo")
