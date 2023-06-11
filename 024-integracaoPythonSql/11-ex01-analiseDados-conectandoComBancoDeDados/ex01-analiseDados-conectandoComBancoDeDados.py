e4b7adc1c0cb37d094723473b52aa19f59aa2217
Jorge Harbes <jharbes@hotmail.com>
2023-06-11 09:02:20 -0300

    Create ex01-analiseDados-conectandoComBancoDeDados.py

---
+++ 024-integracaoPythonSql/11-ex01-analiseDados-conectandoComBancoDeDados/ex01-analiseDados-conectandoComBancoDeDados.py
@@ -0,0 +1,17 @@
+"""
+## Exercício Desafio
+
+- Digamos que seu chefe pediu para você um relatório da análise dos salários da unidade de San Francisco da empresa. O objetivo dele é entender:
+
+1. Qual foi a evolução do salário médio ao longo dos anos (TotalPay e TotalPayBenefits)
+
+2. Quantos funcionários tivemos ao longo dos anos
+
+3. Qual foi a evolução do total gasto com salário ao longo dos anos (TotalPayBenefits)
+
+- Base de Dados a ser usada: salarios.sqlite
+
+"""
+
+### Importação da Base de Dados
+
