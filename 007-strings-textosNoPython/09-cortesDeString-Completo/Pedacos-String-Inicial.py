2fa7d237c92f642564c8178654cd6f867f2ea1ad
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-12 11:19:32 -0300

    pedacos e recortes strings fim

--- 007-strings-textosNoPython/09-cortesDeString-Completo/Pedacos-String-Inicial.py
+++ 007-strings-textosNoPython/09-cortesDeString-Completo/Pedacos-String-Inicial.py
@@ -5,28 +5,38 @@
 
 # In[ ]:
 
+precos = "Jan: 25, Fev: 27, Mar: 29"
 
 precos = "Jan: 25, Fev: 27, Mar: 29"
 
+precos_lista=precos.split(',')
+print(precos_lista)
+
+precos_lista=[item[-2:] for item in precos_lista]
+print(precos_lista)
 
 # ### Posição Inicial e Final
 
-# In[ ]:
 
-
-
-
-
-# ### Posição Inicial e Final com Step
 
 # In[ ]:
 
+# STEP NAS STRINGS
+
+# posicoes [x,y,z]
+# x -> posicao inicial do recorte
+# y -> posicao final do recorte
+# z -> STEP - de quantos em quantos ele vai buscar
 
 codigo = "1.2.3.4,5,1,2.3.4,7.9"
+num_codigos = codigo[-1::-1]
+
+print(num_codigos) # 9.7,4.3.2,1,5,4.3.2.1
 
 
-# In[ ]:
 
+num_codigos = codigo[10:2:-2]
 
+print(num_codigos) # 1543
 
 
