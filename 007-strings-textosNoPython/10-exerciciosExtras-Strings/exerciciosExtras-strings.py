a70ae84f98e6acffe5c992ecfb5b53d05d5b95fb
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-12 11:22:12 -0300

    Create exerciciosExtras-strings.py

---
+++ 007-strings-textosNoPython/10-exerciciosExtras-Strings/exerciciosExtras-strings.py
@@ -0,0 +1,32 @@
+"""
+# Strings
+
+Todos os exercícios são feitos partindo-se do pressuposto de que todas as entradas são dadas de forma correta. Casos limite não mencionados no enunciado não são abordados porque não fazem parte do exercício.
+
+"""
+#### 1. Tamanho de strings. Faça um programa que leia 2 strings e informe o conteúdo delas seguido do seu comprimento. Informe também se as duas strings possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
+
+# Compara duas strings
+# String 1: Brasil Hexa 2006
+# String 2: Brasil! Hexa 2006!
+# Tamanho de "Brasil Hexa 2006": 16 caracteres
+# Tamanho de "Brasil! Hexa 2006!": 18 caracteres
+# As duas strings são de tamanhos diferentes.
+# As duas strings possuem conteúdo diferente.
+
+
+
+
+
+
+
+
+
+
+#### 2. Valida e corrige número de telefone. Faça um programa que leia um número de telefone, e corrija o número no caso deste conter somente 7 dígitos, acrescentando o '3' na frente. O usuário pode informar o número com ou sem o traço separador.
+
+# Valida e corrige número de telefone
+# Telefone: 461-0133
+# Telefone possui 7 dígitos. Vou acrescentar o digito três na frente.
+# Telefone corrigido sem formatação: 34610133
+# Telefone corrigido com formatação: 3461-0133
