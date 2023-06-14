c4258b70b9bdd8bdfeeaa45ecbd936c61ab762e8
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-14 11:01:51 -0300

    06 juntar listar e ordenar fim

--- 008-listas-metodosEUsos/06-justarListasEOrdenar/listas_06.py
+++ 008-listas-metodosEUsos/06-justarListasEOrdenar/listas_06.py
@@ -19,6 +19,15 @@ Obs: o Método .append não junta listas, mas adiciona um valor no final da list
 produtos = ['apple tv', 'mac', 'iphone x', 'iphone 11', 'IPad', 'apple watch', 'mac book', 'airpods']
 novos_produtos = ['iphone 12', 'ioculos']
 
+lista_nova=produtos+novos_produtos
+print(lista_nova) # ['apple tv', 'mac', 'iphone x', 'iphone 11', 'IPad', 'apple watch', 'mac book', 'airpods', 'iphone 12', 'ioculos']
+
+produtos.extend(novos_produtos)
+print(produtos) # ['apple tv', 'mac', 'iphone x', 'iphone 11', 'IPad', 'apple watch', 'mac book', 'airpods', 'iphone 12', 'ioculos']
+
+
+
+
 """### Cuidado:
 
 - [1] + [2] não é a mesma coisa que 1 + 2, então cuidado sempre com o formato dos valores na hora de fazer operações.
@@ -27,6 +36,12 @@ novos_produtos = ['iphone 12', 'ioculos']
 vendas = [1000, 1500, 15000, 20000, 270, 900, 100, 1200]
 vendas_iphonex = [15000]
 vendas_iphone11 = [20000]
+
+print([1]+[2]) # [1, 2]
+print(1+2) # 3
+
+
+
 
 """### Ordenar listas
 
