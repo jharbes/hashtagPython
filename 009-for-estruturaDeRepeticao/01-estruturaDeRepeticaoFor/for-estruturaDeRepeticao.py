467231e6ec4f26eff7cdde21db61065030fbba78
Jorge Harbes <jharbes@hotmail.com>
2023-06-15 18:46:56 -0300

    Create for-estruturaDeRepeticao.py

---
+++ 009-for-estruturaDeRepeticao/01-estruturaDeRepeticaoFor/for-estruturaDeRepeticao.py
@@ -0,0 +1,14 @@
+"""
+# Estrutura de Repetição: for
+
+### Funcionamento:
+
+for i in range(n):
+    repetir código n vezes
+
+"""
+
+# - Imagine que você está construindo uma automação para enviar todo dia por e-mail um resumo da produção de uma fábrica. Construa um código que exiba a quantidade produzida de cada os produto nesse "e-mail"
+
+produtos = ['coca', 'pepsi', 'guarana', 'sprite', 'fanta']
+producao = [15000, 12000, 13000, 5000, 250]
