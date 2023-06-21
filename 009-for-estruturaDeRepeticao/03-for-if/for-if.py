6ade1b3c08897e6284deef40776d1c79beca5e11
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-21 09:17:46 -0300

    Create for-if.py

---
+++ 009-for-estruturaDeRepeticao/03-for-if/for-if.py
@@ -0,0 +1,22 @@
+# for + if
+
+### Estrutura:
+
+"""
+for item in lista:
+    if condicao:
+        faça alguma coisa
+    else:
+        outra coisa
+
+"""
+
+
+"""
+Digamos que a gente esteja analisando a meta de vendas de vários funcionários de uma empresa. A meta de vendas é de 1000 reais em 1 dia.
+
+Temos uma lista com as vendas de todos os funcionários e quero calcular qual o % de pessoas que bateram a meta.
+
+"""
+vendas = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450, 800]
+meta = 1000
