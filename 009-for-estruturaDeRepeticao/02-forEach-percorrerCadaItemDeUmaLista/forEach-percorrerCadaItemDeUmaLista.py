5b5267efad9afcd4628a78a5cc1e26aaef8fbfc5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-21 09:03:39 -0300

    Create forEach-percorrerCadaItemDeUmaLista.py

---
+++ 009-for-estruturaDeRepeticao/02-forEach-percorrerCadaItemDeUmaLista/forEach-percorrerCadaItemDeUmaLista.py
@@ -0,0 +1,29 @@
+# For "each"
+
+### Estrutura:
+
+# O for no Python consegue percorrer uma lista e a cada "loop" retornar o valor do item.
+
+
+"""
+for i in range(5):
+    print(i)
+    
+range(5) é na verdade uma lista do tipo: [0, 1, 2, 3, 4]
+
+"""
+
+"""
+for item in lista:
+    print(item)
+    
+ou então para string:
+
+for ch in texto:
+    print(ch)
+
+"""
+
+produtos = ['coca', 'pepsi', 'guarana', 'sprite', 'fanta']
+texto = 'lira@gmail.com'
+
