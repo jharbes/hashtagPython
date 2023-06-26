3c06e5cceabd2b5d2c7d723d3bc7a072bc19c067
Jorge Harbes <jharbes@hotmail.com>
2023-06-26 18:41:53 -0300

    Create enumerate-forComItemEIndice.py

---
+++ 009-for-estruturaDeRepeticao/04-enumerate-forComItemEIndice/enumerate-forComItemEIndice.py
@@ -0,0 +1,16 @@
+"""
+# Enumerate
+
+### Estrutura:
+
+O enumerate permite que você percorra uma lista e ao mesmo tempo tenha em uma variável o índice daquele item.
+
+- for normalmente
+
+for item in lista:
+    resto do código
+"""
+
+funcionarios = ['Maria', 'José', 'Antônio', 'João', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos', 'Manoel', 'Pedro', 'Francisca', 'Marcos', 'Raimundo', 'Sebastião', 'Antônia', 'Marcelo', 'Jorge', 'Márcia', 'Geraldo', 'Adriana', 'Sandra', 'Luis']
+for funcionario in funcionarios:
+    print(funcionario)
