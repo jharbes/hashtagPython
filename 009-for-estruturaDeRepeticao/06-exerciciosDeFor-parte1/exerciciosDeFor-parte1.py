d49637876e5979053fc8550e5daed7d5c9b9ae01
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-27 09:58:49 -0300

    exercicio 2 fim

--- 009-for-estruturaDeRepeticao/06-exerciciosDeFor-parte1/exerciciosDeFor-parte1.py
+++ 009-for-estruturaDeRepeticao/06-exerciciosDeFor-parte1/exerciciosDeFor-parte1.py
@@ -37,3 +37,26 @@ except:
 
 
 
+"""
+## 2. Análise de Vendas
+
+Nesse exercício vamos fazer uma "análise simples" de atingimento de Meta.
+
+Temos uma lista com os vendedores e os valores de vendas e queremos identificar (printar) quais os vendedores que bateram a meta e qual foi o valor que eles venderam.
+
+"""
+
+meta = 10000
+vendas = [
+    ['João', 15000],
+    ['Julia', 27000],
+    ['Marcus', 9900],
+    ['Maria', 3750],
+    ['Ana', 10300],
+    ['Alon', 7870],
+]
+
+print('Vendedores que bateram as metas com seu respectivo valor em vendas:')
+for vendedor in vendas:
+    if vendedor[1]>10000:
+        print('Vendedor {} com R${} em vendas'.format(vendedor[0],vendedor[1]))
