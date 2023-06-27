815255cd2198280e75b6f757471c95a586b229f5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-27 08:12:15 -0300

    05 quando usar cada tipo de for fim

--- 009-for-estruturaDeRepeticao/05-quandoUsarCadaTipoDeFor/diferentesFor.py
+++ 009-for-estruturaDeRepeticao/05-quandoUsarCadaTipoDeFor/diferentesFor.py
@@ -14,7 +14,8 @@ precos = [7000, 10000, 2500, 14000]
 
 
 # preco com imposto
-
+for preco in precos:
+    print(preco*1.1)
 
 # ### For i in range
 
@@ -22,7 +23,8 @@ precos = [7000, 10000, 2500, 14000]
 
 
 # preco de cada produto
-
+for i in range(len(precos)):
+    print(produtos[i],precos[i])
 
 # ### For item in lista com enumerate
 
@@ -30,7 +32,8 @@ precos = [7000, 10000, 2500, 14000]
 
 
 # preco de cada produto com imposto
-
+for i, preco in enumerate(precos):
+    print(produtos[i],preco)
 
 # In[ ]:
 
