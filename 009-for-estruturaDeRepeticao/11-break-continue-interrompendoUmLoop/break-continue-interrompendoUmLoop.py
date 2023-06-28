2e7734d93650f45386b0bb0f11e14362f71cb61c
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-28 12:52:53 -0300

    Create break-continue-interrompendoUmLoop.py

---
+++ 009-for-estruturaDeRepeticao/11-break-continue-interrompendoUmLoop/break-continue-interrompendoUmLoop.py
@@ -0,0 +1,23 @@
+# Formas de interromper um for
+
+### 2 Opções:
+
+# - break -> interrompe e finaliza o for
+# - continue -> interrompe e vai para o próximo item do for
+
+vendas = [100, 150, 1500, 2000, 120]
+
+
+
+
+# - Caso 1: Se todas as vendas forem acima da meta, a loja ganha bônus
+
+meta = 110
+
+
+
+
+# - Caso 2: Exiba quem bateu a meta
+
+vendedores = ['João', 'Julia', 'Ana', 'José', 'Maria']
+meta = 130
