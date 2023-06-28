687fe5c30e2d762cb18f4cb2747e81f0e707e8a0
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-28 11:08:18 -0300

    exercicios de for, parte3 inicio

---
+++ 009-for-estruturaDeRepeticao/10-exerciciosDeFor-parte3/exerciciosDeFor-parte3.py
@@ -0,0 +1,20 @@
+"""
+# Exercícios
+
+## 1. Calculando % de uma lista
+
+Faremos algo parecido com "filtrar" uma lista. Mais pra frente no curso aprenderemos outras formas de fazer isso, mas com o nosso conhecimentoa atual já conseguimos resolver o desafio.
+
+Digamos que a gente tenha uma lista de vendedores e ao invés de saber todos os vendedores que bateram a meta, eu quero conseguir calcular o % de vendedores que bateram a meta. Ou seja, se temos 10 vendedores e 3 bateram a meta, temos 30% dos vendedores que bateram a meta.
+
+"""
+
+meta = 10000
+vendas = [
+    ['João', 15000],
+    ['Julia', 27000],
+    ['Marcus', 9900],
+    ['Maria', 3750],
+    ['Ana', 10300],
+    ['Alon', 7870],
+]
