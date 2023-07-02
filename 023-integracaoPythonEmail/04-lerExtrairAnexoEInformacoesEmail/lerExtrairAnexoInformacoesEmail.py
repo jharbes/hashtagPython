4944523025bb98a27ff02afc00a47dc52b617bad
Jorge Harbes <jharbes@hotmail.com>
2023-07-02 14:56:52 -0300

    ler e extrair anexos e informacoes email 04 fim

--- 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
+++ 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
@@ -16,14 +16,15 @@ print(sys.path) # imprimindo todos os sys.path disponiveis após a adicao acima
 # importacao da senha em arquivo nao disponivel no github
 from passwd import password
 
-from imap_tools import MailBox, AND
+# AND, OR e NOT sao condicoes que podem ser utilizadas no FETCH para filtrar os emails disponiveis e desejados
+from imap_tools import MailBox, AND, OR, NOT
 
 usuario='jharbes@hotmail.com'
 senha=password
 
 # TODO: testando o todo
 
-meu_mail=MailBox('outlook.office365.com').login(usuario,senha)
+meu_email=MailBox('outlook.office365.com').login(usuario,senha)
 
 
 
@@ -37,3 +38,25 @@ https://github.com/ikvk/imap_tools#search-criteria
 
 # pegar email que foram enviados por um remetente específico
 
+lista_emails = meu_email.fetch(AND(from_="jorge.harbes@technipfmc.com", to="jharbes@hotmail.com"))
+
+for email in lista_emails:
+    print(email.subject)
+    print(email.text)
+
+
+
+
+# pegar anexo de um email
+
+lista_emails = meu_email.fetch(AND(from_="jorgenamiharbes@gmail.com"))
+for email in lista_emails:
+    if len(email.attachments) > 0:
+        for anexo in email.attachments:
+            if "RelatorioExcel" in anexo.filename:
+                # att.payload  # bytes: b'\xff\xd8\xff\xe0\' - cria uma copia escrevendo o binario do arquivo
+                informacoes_anexo = anexo.payload
+
+                # usaremos o with no python para recriar o arquivo excel (ou qualquer outro tipo) por meio do seu binario extraido com o payload
+                with open("RelatorioExceldoEmail.xlsx", "wb") as arquivo_excel:
+                    arquivo_excel.write(informacoes_anexo)
