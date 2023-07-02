57957f8734f8ed73215c4429503e4f6c836a0853
Jorge Harbes <jharbes@hotmail.com>
2023-07-02 13:56:58 -0300

    integracao python email 04 commit 2

--- 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
+++ 023-integracaoPythonEmail/04-lerExtrairAnexoEInformacoesEmail/lerExtrairAnexoInformacoesEmail.py
@@ -3,10 +3,17 @@ Ler e extrair anexo e informacoes do Email
 
 """
 
+"""
+Lista de sites com servidores IMAP de vários tipos de email:
+https://www.systoolsgroup.com/imap/
+
+"""
+
 import sys
 sys.path.append('C:\\Users\\Jorge\\Desktop\\hashtag')
-print(sys.path)
+print(sys.path) # imprimindo todos os sys.path disponiveis após a adicao acima
 
+# importacao da senha em arquivo nao disponivel no github
 from passwd import password
 
 from imap_tools import MailBox, AND
@@ -16,4 +23,17 @@ senha=password
 
 # TODO: testando o todo
 
-meuEmail=MailBox('outlook.office365.com').login(usuario,senha)
+meu_mail=MailBox('outlook.office365.com').login(usuario,senha)
+
+
+
+
+"""
+Link com ferramentas do IMAP tools com filtros que podem ser feitos para encontrar e-mails na caixa de entrada usando o fetch:
+
+https://github.com/ikvk/imap_tools#search-criteria
+
+"""
+
+# pegar email que foram enviados por um remetente específico
+
