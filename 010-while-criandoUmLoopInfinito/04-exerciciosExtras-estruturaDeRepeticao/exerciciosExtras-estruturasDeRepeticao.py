4298411615decc01082632d66566c0ee565f59c4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-03 09:20:40 -0300

    Create exerciciosExtras-estruturasDeRepeticao.py

---
+++ 010-while-criandoUmLoopInfinito/04-exerciciosExtras-estruturaDeRepeticao/exerciciosExtras-estruturasDeRepeticao.py
@@ -0,0 +1,8 @@
+"""
+# Estruturas de repetição
+
+Todos os exercícios são feitos partindo-se do pressuposto de que todas as entradas são dadas de forma correta. Casos limite não mencionados no enunciado não são abordados porque não fazem parte do exercício.
+
+"""
+
+#### 1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
