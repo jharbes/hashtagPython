c7adc0b39a9f47a646f49b571c41beea32bd0c7a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-03 11:18:08 -0300

    ex01 resolvido

--- 010-while-criandoUmLoopInfinito/04-exerciciosExtras-estruturaDeRepeticao/exerciciosExtras-estruturasDeRepeticao.py
+++ 010-while-criandoUmLoopInfinito/04-exerciciosExtras-estruturaDeRepeticao/exerciciosExtras-estruturasDeRepeticao.py
@@ -6,3 +6,16 @@ Todos os exercícios são feitos partindo-se do pressuposto de que todas as entr
 """
 
 #### 1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+while True:
+    nota=input('Digite uma nota entre 0 e 10: ')
+    try:
+        nota=int(nota)
+        if nota>=0 and nota<=10:
+            print('\nNota {} válida!'.format(nota))
+            break
+        else:
+            print('\nVALOR INVÁLIDO!')
+            continue
+    except:
+        print('\nVALOR INVÁLIDO!')
