61ac82929a29e1eeb107e45ae6f161c46c877e56
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-03 12:35:42 -0300

    ex04 resolvido

--- 010-while-criandoUmLoopInfinito/04-exerciciosExtras-estruturaDeRepeticao/exerciciosExtras-estruturasDeRepeticao.py
+++ 010-while-criandoUmLoopInfinito/04-exerciciosExtras-estruturaDeRepeticao/exerciciosExtras-estruturasDeRepeticao.py
@@ -93,3 +93,20 @@ while estado_civil!='s' and estado_civil!='c' and estado_civil!='v' and estado_c
 
 #### 4. Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
 
+populacao_A=80000
+populacao_B=200000
+
+numero_anos=0
+
+while populacao_A<=populacao_B:
+    populacao_A*=1.03
+    populacao_B*=1.015
+    numero_anos+=1
+
+print('O número de anos necessários serão de {}.'.format(numero_anos))
+
+
+
+
+#### 5. Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+
