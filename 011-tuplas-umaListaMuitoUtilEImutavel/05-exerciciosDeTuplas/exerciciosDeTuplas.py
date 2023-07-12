d2211e468fb3ca09eee1378b32c964c4428fc666
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-12 10:53:36 -0300

    Create exerciciosDeTuplas.py

---
+++ 011-tuplas-umaListaMuitoUtilEImutavel/05-exerciciosDeTuplas/exerciciosDeTuplas.py
@@ -0,0 +1,22 @@
+"""
+# Exercícios
+
+São exercícios bem parecidos com os que fizemos com listas. Mas na tupla, podemos não só trabalhar com índices, mas fazer o "unpacking" das tuplas, o que pode facilitar nossos códigos.
+
+## 1. Análise de Vendas
+
+Nesse exercício vamos fazer uma "análise simples" de atingimento de Meta.
+
+Temos uma lista com os vendedores e os valores de vendas e queremos identificar (printar) quais os vendedores que bateram a meta e qual foi o valor que eles venderam.
+
+"""
+
+meta = 10000
+vendas = [
+    ('João', 15000),
+    ('Julia', 27000),
+    ('Marcus', 9900),
+    ('Maria', 3750),
+    ('Ana', 10300),
+    ('Alon', 7870),
+]
