9835dbed45e1202222d9affe6928f02a452a23c3
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-13 07:09:53 -0300

    dicionarios 03 inicio

---
+++ 012-dicionariosEmPython/03-adicionarModificarRemoverItensDicionario/adicionarModificarRemoverItensDicionario.py
@@ -0,0 +1,24 @@
+"""
+# Adicionar, Remover e Modificar Itens no Dicionário
+
+### Estrutura:
+
+- Adicionar itens:
+
+dicionario = {}
+
+dicionario[chave] = valor
+
+outra opção:
+
+dicionario.update({chave: valor, chave: valor})
+
+"""
+
+lucro_1tri = {'janeiro': 100000, 'fevereiro': 120000, 'março': 90000}
+lucro_2tri = {'abril': 88000, 'maio': 89000, 'junho': 120000}
+#adicionando 1 item
+
+#adicionando vários itens ou um dicionário a outro
+
+#adicionando um item já existente (manualmente ou pelo update)
