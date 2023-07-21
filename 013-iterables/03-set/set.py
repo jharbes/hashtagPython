84392261961bb2fb404f7b0db056d0a3a449d330
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-21 13:34:20 -0300

    03 set finalizado

--- 013-iterables/03-set/set.py
+++ 013-iterables/03-set/set.py
@@ -16,6 +16,11 @@ set_produtos = {'arroz', 'feijao', 'macarrao', 'atum', 'azeite'}
 
 print(set_produtos) # {'azeite', 'macarrao', 'arroz', 'feijao', 'atum'}
 
+novo_set=set()
+print(novo_set)
+novo_set.add(1)
+print(novo_set)
+
 
 
 
@@ -24,7 +29,10 @@ print(set_produtos) # {'azeite', 'macarrao', 'arroz', 'feijao', 'atum'}
     # 1. Quantos clientes tivemos na loja?
 
 cpf_clientes = ['762.196.080-97', '263.027.380-67', '827.363.930-40', '925.413.640-91', '870.565.160-33', '892.080.930-50', '462.126.030-81', '393.462.330-10', '393.462.330-10', '393.462.330-10', '988.305.810-11', '596.125.830-05', '596.125.830-05', '990.236.770-48']
+print('Tamanho da lista cpf_clientes: {}'.format(len(cpf_clientes)))
 
+
+# podemos usar essa estrategia para remover os itens duplicados de uma lista tornado-a um set e depois torna-la de novo uma lista com a funcao list()
 cpf_clientes_set=set(cpf_clientes)
 
 print(cpf_clientes_set)
@@ -32,4 +40,4 @@ print(cpf_clientes_set)
 for item in cpf_clientes_set:
     print(item)
 
-print('\n{} Clientes'.format(len(cpf_clientes_set)))
+print('\nTamanho set cpf_clientes_set: {}'.format(len(cpf_clientes_set)))
