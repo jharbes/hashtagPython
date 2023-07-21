302118de98c7dd90523507f73eae6b684130ea26
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-21 13:22:02 -0300

    03 set commit 1

---
+++ 013-iterables/03-set/set.py
@@ -0,0 +1,35 @@
+"""
+# Set
+
+### Estrutura:
+
+meu_set = {valor, valor, valor, valor,...}
+
+### Observações:
+
+- Não pode ter valores duplicados
+- Não tem ordem fixa
+
+"""
+
+set_produtos = {'arroz', 'feijao', 'macarrao', 'atum', 'azeite'}
+
+print(set_produtos) # {'azeite', 'macarrao', 'arroz', 'feijao', 'atum'}
+
+
+
+
+# - Aplicação bem útil:
+
+    # 1. Quantos clientes tivemos na loja?
+
+cpf_clientes = ['762.196.080-97', '263.027.380-67', '827.363.930-40', '925.413.640-91', '870.565.160-33', '892.080.930-50', '462.126.030-81', '393.462.330-10', '393.462.330-10', '393.462.330-10', '988.305.810-11', '596.125.830-05', '596.125.830-05', '990.236.770-48']
+
+cpf_clientes_set=set(cpf_clientes)
+
+print(cpf_clientes_set)
+
+for item in cpf_clientes_set:
+    print(item)
+
+print('\n{} Clientes'.format(len(cpf_clientes_set)))
