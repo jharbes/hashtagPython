a83b6133bb60e56f1075d7cc80df9d4ac3e4606a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-21 13:13:43 -0300

    range 02 finalizado

--- 013-iterables/02-range/range.py
+++ 013-iterables/02-range/range.py
@@ -19,6 +19,9 @@ ou
 produtos = ['arroz', 'feijao', 'macarrao', 'atum', 'azeite']
 estoque = [50, 100, 20, 5, 80]
 
+for i in range(0,len(produtos)):
+    print('{} : {}'.format(produtos[i],estoque[i]))
+
 
 # range com inicio e fim
 print(range(1, 10))
@@ -41,7 +44,9 @@ for i in range(1,10):
 funcionarios = ['Maria', 'José', 'Antônio', 'João', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos', 'Manoel', 'Pedro', 'Francisca', 'Marcos', 'Raimundo', 'Sebastião', 'Antônia', 'Marcelo', 'Jorge', 'Márcia', 'Geraldo']
 vendas = [2750, 1900, 1500, 1200, 1111, 1100, 999, 900, 880, 870, 800, 800, 450, 400, 300, 300, 120, 90, 80, 70]
 
-
+print('Funcionários Classe B\n')
+for i in range(3,18):
+    print('Funcionário {}'.format(funcionarios[i]))
 
 
 # range com passo
