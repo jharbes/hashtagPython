072a4119dfefd76f8eca2b568eb928511f7d57f7
Jorge Harbes <jharbes@hotmail.com>
2023-07-24 17:17:30 -0300

    funcoes 03 commit 1

---
+++ 014-criandoSuasFuncoesEmPython/03-argumentos-parametros-emUmaFunction/argumentos-parametros-emUmaFunction.py
@@ -0,0 +1,23 @@
+"""
+# Argumentos/Parâmetros da Função
+
+### Estrutura:
+
+def minha_funcao(parametro1, parametro2, parametro3):
+    return parametro1 + parametro2 + parametro3
+
+"""
+
+# - Exemplo: nosso famoso print
+
+print('texto')
+
+
+
+
+# - Só para mostrar o funcionamento, vamos criar uma função de soma
+
+def soma(x,y):
+    return x+y
+
+print(soma(2,3))
