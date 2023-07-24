0d8111c3c77c8629433330154b17b2fa7c0db77d
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-24 08:05:54 -0300

    Create retornarUmValorNaFunction.py

---
+++ 014-criandoSuasFuncoesEmPython/02-retornarUmValorNaFunction/retornarUmValorNaFunction.py
@@ -0,0 +1,17 @@
+"""
+# Retornar um valor na Function Python
+
+### Estrutura Básica
+
+def nome_funcao():
+    return valor_final
+
+"""
+
+# - Exemplo: vamos criar uma função de cadastro de um Produto. Essa função deve garantir que o produto cadastrado está em letra minúscula.
+
+def cadastrar_produto():
+    produto = input('Digite o nome do produto que deseja cadastrar')
+    produto = produto.casefold()
+    produto = produto.strip()
+    return produto
