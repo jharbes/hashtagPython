ce12200c3ba2e05ea67f1d4b7876f3c0aa735de9
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-26 07:10:02 -0300

    function 07 fim

--- 014-funcoesEmPython/07-dicas-exemplos-argumentosEmFunctions/dicas-exemplos-argumentosEmFunctions.py
+++ 014-funcoesEmPython/07-dicas-exemplos-argumentosEmFunctions/dicas-exemplos-argumentosEmFunctions.py
@@ -18,9 +18,20 @@ def eh_da_categoria(bebida, cod_categoria):
     
 
 
-#texto para upper
+# texto para upper
 cod_produto = 'beb12304'
+print(cod_produto.upper()) # BEB12304
+print(cod_produto) # beb12304
 
 # lista para sort e extend
 vendas_ano = [100, 200, 50, 90, 240, 300, 55, 10, 789, 60]
 vendas_novdez = [500, 1555]
+
+vendas_ano.sort()
+print(vendas_ano) # [10, 50, 55, 60, 90, 100, 200, 240, 300, 789]
+
+vendas_ano.sort(reverse=True)
+print(vendas_ano) # [789, 300, 240, 200, 100, 90, 60, 55, 50, 10]
+
+vendas_ano.extend(vendas_novdez)
+print(vendas_ano) # [789, 300, 240, 200, 100, 90, 60, 55, 50, 10, 500, 1555]
