59eea16f5c34b482fba0b52757e2bdf9a3bc039f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-28 08:39:49 -0300

    function 013 commit 3

--- 014-funcoesEmPython/13-desafioReturnComVariosObjetos/desafioReturnComVariosObjetos.py
+++ 014-funcoesEmPython/13-desafioReturnComVariosObjetos/desafioReturnComVariosObjetos.py
@@ -18,13 +18,67 @@ tamanho_imoveis = [207,148,130,203,257,228,160,194,232,147]
 
 # Vamos definir qual o fator que vamos dividir as listas (ou seja, quantos % da lista vai ficar pra teste. O resto fica pra treino)
 # Vamos usar 0.1 (10%)
+percentual_teste=0.1
+
 
 # Isso significa que a lista de teste tem quantos itens?
+numero_itens_teste=percentual_teste*len(precos_imoveis)
+print(numero_itens_teste) # 1
+
 
 # Agora vamos entender qual conta temos que fazer para dividir a lista em 2 listas. Uma com 90% dos valores e outra com 10%
+precos_imoveis_treino=[]
+precos_imoveis_teste=[]
+tamanho_imoveis_treino=[]
+tamanho_imoveis_teste=[]
+
+for i in range(len(precos_imoveis)):
+    if i<numero_itens_teste:
+        precos_imoveis_teste.append(precos_imoveis[i])
+        tamanho_imoveis_teste.append(tamanho_imoveis[i])
+    else:
+        precos_imoveis_treino.append(precos_imoveis[i])
+        tamanho_imoveis_treino.append(tamanho_imoveis[i])
+
+
+
+print(precos_imoveis_treino) # [1.54, 1.45, 1.94, 2.37, 2.3, 1.79, 1.8, 2.25, 1.37]
+
+print(precos_imoveis_teste) # [2.17]
+
+print(tamanho_imoveis_treino) # [148, 130, 203, 257, 228, 160, 194, 232, 147]
+
+print(tamanho_imoveis_teste) # [207]
 
 
 
 
 precos_imoveis = [2.17,1.54,1.45,1.94,2.37,2.3,1.79,1.8,2.25,1.37,2.4,1.72,2,1.69,1.63,2.01,2.25,1.61,1.02,1.19,1.86,2.15,2.03,1.61,1.52,1.56,1.69,1.47,1.09,2.47,1.62,2.15,1.81,2.49,2.08,1.02,1.68,1.53,1.2,1.29,1.88,1.92,2.14,1.95,2.48,2.44,1.41,1.98,1.89,1.69,1.95,1.42,1.57,2.32,1.23,1.43,1.35,1.49,2.39,2.37,1.3,2.25,1.5,1.35,2.06,1.05,1.7,2.29,2.44,2.09,1.81,2.04,2.45,1.42,2.09,2.19,2.09,1,2.23,1.39,2,1.29,1.55,1.67,2.06,1.89,2.07,2.39,1.93,1.51,1.73,1.66,1.18,1.13,1.69,2.48,1.26,1.75, 1.51, 1.73]
 tamanho_imoveis = [207,148,130,203,257,228,160,194,232,147,222,165,184,175,147,217,214,171,86,111,180,211,210,168,156,154,179,163,99,246,162,205,195,263,198,121,149,140,122,119,197,210,218,202,258,256,135,203,173,152,197,145,154,252,141,141,151,133,232,229,134,215,155,138,186,120,152,213,256,219,200,210,238,140,224,233,222,120,233,151,185,111,149,186,194,194,222,223,185,157,154,164,129,128,169,240,136,191, 157, 154]
+
+
+numero_itens_teste=int(percentual_teste*len(precos_imoveis))
+print(numero_itens_teste) # 10
+
+precos_imoveis_treino=[]
+precos_imoveis_teste=[]
+tamanho_imoveis_treino=[]
+tamanho_imoveis_teste=[]
+
+for i in range(len(precos_imoveis)):
+    if i<numero_itens_teste:
+        precos_imoveis_teste.append(precos_imoveis[i])
+        tamanho_imoveis_teste.append(tamanho_imoveis[i])
+    else:
+        precos_imoveis_treino.append(precos_imoveis[i])
+        tamanho_imoveis_treino.append(tamanho_imoveis[i])
+
+
+
+print(precos_imoveis_treino) # [2.4, 1.72, 2, 1.69, 1.63, 2.01, 2.25, 1.61, 1.02, 1.19, 1.86, 2.15, 2.03, 1.61, 1.52, 1.56, 1.69, 1.47, 1.09, 2.47, 1.62, 2.15, 1.81, 2.49, 2.08, 1.02, 1.68, 1.53, 1.2, 1.29, 1.88, 1.92, 2.14, 1.95, 2.48, 2.44, 1.41, 1.98, 1.89, 1.69, 1.95, 1.42, 1.57, 2.32, 1.23, 1.43, 1.35, 1.49, 2.39, 2.37, 1.3, 2.25, 1.5, 1.35, 2.06, 1.05, 1.7, 2.29, 2.44, 2.09, 1.81, 2.04, 2.45, 1.42, 2.09, 2.19, 2.09, 1, 2.23, 1.39, 2, 1.29, 1.55, 1.67, 2.06, 1.89, 2.07, 2.39, 1.93, 1.51, 1.73, 1.66, 1.18, 1.13, 1.69, 2.48, 1.26, 1.75, 1.51, 1.73]
+
+print(precos_imoveis_teste) # [2.17, 1.54, 1.45, 1.94, 2.37, 2.3, 1.79, 1.8, 2.25, 1.37]
+
+print(tamanho_imoveis_treino) # [222, 165, 184, 175, 147, 217, 214, 171, 86, 111, 180, 211, 210, 168, 156, 154, 179, 163, 99, 246, 162, 205, 195, 263, 198, 121, 149, 140, 122, 119, 197, 210, 218, 202, 258, 256, 135, 203, 173, 152, 197, 145, 154, 252, 141, 141, 151, 133, 232, 229, 134, 215, 155, 138, 186, 120, 152, 213, 256, 219, 200, 210, 238, 140, 224, 233, 222, 120, 233, 151, 185, 111, 149, 186, 194, 194, 222, 223, 185, 157, 154, 164, 129, 128, 169, 240, 136, 191, 157, 154]
+
+print(tamanho_imoveis_teste) # [207, 148, 130, 203, 257, 228, 160, 194, 232, 147]
