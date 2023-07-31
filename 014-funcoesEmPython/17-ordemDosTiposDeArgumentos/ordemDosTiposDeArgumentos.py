b806bb98b5d234b8dfe87b4c9370c95a4a2656df
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-31 08:28:07 -0300

    Create ordemDosTiposDeArgumentos.py

---
+++ 014-funcoesEmPython/17-ordemDosTiposDeArgumentos/ordemDosTiposDeArgumentos.py
@@ -0,0 +1,12 @@
+"""
+# Ordem dos Argumentos
+
+### Estrutura:
+
+- Sempre os positional arguments vêm antes e depois os keywords arguments.
+- Sempre os argumentos individuais vêm antes e depois os "múltiplos"
+
+def minha_funcao(arg1, arg2, arg3, arg4, *args, k = kwarg1, k2 = kwarg2, k3 = kwarg3, **kwargs):
+    ...
+
+"""
