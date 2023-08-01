7155ca6a1dfc3f1c7fd454d7348377c85c184b78
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-01 08:19:00 -0300

    Create modulos-introducaoPOO2.py

---
+++ 015-modulos-bibliotecas-introducaoPOO/02-oQueSaoModulos/modulos-introducaoPOO2.py
@@ -0,0 +1,20 @@
+"""
+# O que são Módulos e qual a importância deles?
+
+### Importância
+
+- Já tem muita coisa pronta, então você não precisa criar do zero.
+- Se você souber usar Módulos e como usar um módulo novo, você vai conseguir fazer praticamente tudo no Python
+
+### Estrutura Básica
+
+import módulo
+
+ou
+
+import módulo as nome
+
+"""
+
+# - Exemplo: Como pode fazer o nosso código abrir um site específico na internet?
+
