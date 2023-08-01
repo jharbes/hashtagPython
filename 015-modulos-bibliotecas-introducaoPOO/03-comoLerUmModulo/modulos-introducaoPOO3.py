3ccea4ef63d0b6dded63e479c5c77269124dd5a5
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-01 12:09:05 -0300

    Create modulos-introducaoPOO3.py

---
+++ 015-modulos-bibliotecas-introducaoPOO/03-comoLerUmModulo/modulos-introducaoPOO3.py
@@ -0,0 +1,15 @@
+"""
+# Começando a ler um módulo
+
+### Vamos testar com o módulo WebBrowser:
+
+- Passo 1: Descobrir qual módulo precisamos usar e qual método do Módulo faz o que a gente quer.
+
+- Passo 2: Se precisarmos de mais alguma coisa do módulo, buscamos por soluções já escolhendo o módulo
+
+"""
+
+import webbrowser
+
+webbrowser.open_new('https://hashtagtreinamentos.com')
+#webbrowser.open('https://www.hashtagtreinamentos.com/blog/')
