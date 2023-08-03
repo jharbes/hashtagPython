19fd527663a00d35709b3ea77e9645cd7295aadd
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-03 14:03:22 -0300

    modulos 07 fim

--- 015-modulos-bibliotecas-introducaoPOO/07-exerciciosTime-parte2/exerciciosTime-parte2.py
+++ 015-modulos-bibliotecas-introducaoPOO/07-exerciciosTime-parte2/exerciciosTime-parte2.py
@@ -26,7 +26,10 @@ import time
 tempo_atual_struct=time.localtime()
 print(tempo_atual_struct) # time.struct_time(tm_year=2023, tm_mon=8, tm_mday=3, tm_hour=13, tm_min=3, tm_sec=19, tm_wday=3, tm_yday=215, tm_isdst=0)
 
-proximo_ano_novo_segundos=time.mktime((2024,1,1,0,0,0,0,0,0))
+
+# essa funcao vai funcionar perfeitamente mesmo com a imprecisao do dado do dia da semana
+# nosso primeiro argumento será tempo_atual_struct.tm_year+1 para que o codigo fique dinamico, permitindo que ele funcione independente do ano em que estejamos
+proximo_ano_novo_segundos=time.mktime((tempo_atual_struct.tm_year+1,1,1,0,0,0,0,0,0))
 print(proximo_ano_novo_segundos) # 1704078000.0
 
 tempo_atual_segundos=time.mktime(tempo_atual_struct)
@@ -43,21 +46,21 @@ diferenca_de_tempo=int(diferenca_de_tempo)
 segundos=diferenca_de_tempo%60
 minutos=diferenca_de_tempo//60
 
-print(segundos)
-print(minutos)
+print(segundos) # 53
+print(minutos) # 216627
 
 
 horas=minutos//60
 minutos=minutos%60
 
-print(minutos)
-print(horas)
+print(minutos) # 27
+print(horas) # 3610
 
 
-dias=horas//24
+dias=horas//24 
 horas=horas%24
 
-print(horas)
-print(dias)
+print(horas) # 10
+print(dias) # 150
 
-print('O tempo para o próximo ano novo é de {} dias, {} horas, {} minutos e {} segundos'.format(dias,horas,minutos,segundos))
+print('O tempo para o próximo ano novo é de {} dias, {} horas, {} minutos e {} segundos'.format(dias,horas,minutos,segundos)) # O tempo para o próximo ano novo é de 150 dias, 10 horas, 27 minutos e 53 segundos
