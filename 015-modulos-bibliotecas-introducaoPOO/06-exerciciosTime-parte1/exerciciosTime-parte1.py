fea4837e252b91df4213ad8ee817a36ea598dc7c
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-03 09:19:34 -0300

    modulo 06 ex01 fim

---
+++ 015-modulos-bibliotecas-introducaoPOO/06-exerciciosTime-parte1/exerciciosTime-parte1.py
@@ -0,0 +1,23 @@
+"""
+## Contagem regressiva
+
+Um evento especial está programado para começar em 10 segundos. Crie uma contagem regressiva que começa em 10 e vai até 0, com uma pausa de um segundo entre cada número.
+
+"""
+
+import time
+
+for i in range(11):
+    time.sleep(1)
+    print(10-i)
+
+
+
+
+"""
+## Formatação de tempo
+
+Uma empresa quer exibir a data e a hora atual em seu site no formato "Dia da semana, dia do mês de mês de ano, horas:minutos". Crie um script Python que mostra a data e a hora atuais neste formato.
+
+"""
+
