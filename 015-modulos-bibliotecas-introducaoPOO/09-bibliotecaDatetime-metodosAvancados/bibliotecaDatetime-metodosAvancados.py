3597c1219cb1678711ad8c0f521dfc0bba486405
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-09 12:13:07 -0300

    modulos 09 commit 3

--- 015-modulos-bibliotecas-introducaoPOO/09-bibliotecaDatetime-metodosAvancados/bibliotecaDatetime-metodosAvancados.py
+++ 015-modulos-bibliotecas-introducaoPOO/09-bibliotecaDatetime-metodosAvancados/bibliotecaDatetime-metodosAvancados.py
@@ -79,3 +79,54 @@ formato = "%m/%d/%Y, %H:%M:%S"
 data = datetime.strptime(string_data, formato)
 
 print(f"Data: {data}") # Data: 2023-09-06 15:30:20
+
+
+
+
+"""
+## Trabalhando com fuso horário
+
+Podemos criar um objeto datetime usando a classe `datetime`. O construtor da classe aceita os seguintes argumentos:
+
+- `year`: ano (por exemplo, 2023)
+- `month`: mês (1-12)
+- `day`: dia (1-31)
+- `hour`: hora (0-23)
+- `minute`: minuto (0-59)
+- `second`: segundo (0-59)
+- `microsecond`: microssegundo (0-999999)
+- `tzinfo`: fuso horário
+
+"""
+from datetime import datetime
+
+data_hora = datetime(2023, 6, 26, 15, 30, 20)
+print(f"Data/hora: {data_hora}") # Data/hora: 2023-06-26 15:30:20
+
+
+
+
+"""
+Os horários que vimos até o momento são os que chamamos de ingênuos (*naive*). Eles não possuem informações sobre o fuso horário. Para criar um horário consciente (*aware*), precisamos passar um objeto `tzinfo` para o construtor da classe `datetime`. O módulo `datetime` fornece uma classe `timezone` que pode ser usada para criar um objeto `tzinfo`. No exemplo abaixo, usamos UTC como fuso horário. UTC significa Tempo Universal Coordenado, que é o fuso horário de referência a partir do qual todos os outros fusos horários são calculados.
+
+![UTC](https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/World_Time_Zones_Map.png/800px-World_Time_Zones_Map.png)
+
+"""
+# exemplo com fuso horário
+from datetime import datetime, timezone
+
+fuso_horario = timezone.utc
+data_hora = datetime(2023, 6, 26, 15, 30, 20, tzinfo=fuso_horario)
+print(f"Data/hora: {data_hora}") # Data/hora: 2023-06-26 15:30:20+00:00
+
+
+
+
+# Podemos passar um objeto `timedelta` para o construtor da classe `timezone` para criar um fuso horário com um deslocamento específico. Por exemplo, o código abaixo cria um fuso horário com um deslocamento de 3 horas em relação ao UTC:
+# exemplo com fuso horário de São Paulo
+
+from datetime import datetime, timezone, timedelta
+
+fuso_horario_sao_paulo = timezone(timedelta(hours=-3))
+data_hora = datetime(2023, 6, 26, 15, 30, 20, tzinfo=fuso_horario_sao_paulo)
+print(f"Data/hora: {data_hora}")
