012181edf2f8e3a1488f91f62e96d082727cb012
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-11 16:36:46 -0300

    modulos 14 commit final

--- 015-modulos-bibliotecas-introducaoPOO/14-maisGraficos-moduloNumpy/maisGraficos-moduloNumpy.py
+++ 015-modulos-bibliotecas-introducaoPOO/14-maisGraficos-moduloNumpy/maisGraficos-moduloNumpy.py
@@ -48,12 +48,70 @@ plt.show()
 
 # mudando a linha para apenas os marcadores (sem a continuidade da linha, apenas os pontos existentes)
 
-plt.plot(meses,vendas,'ro') # define meses no eixo x e vendas no eixo y, o parametro 'ro' define o gradico como marcadores conforme explicado acima
+plt.plot(meses,vendas,'ro') # define meses no eixo x e vendas no eixo y, o parametro 'ro' define o gradico como marcadores conforme explicado acima, todos os tipos de marcadores encontram-se na documentacao do plt.plot()
 
 plt.axis([0,50,0,max(vendas)+200]) # define o dominio de x entre 0 e 50 e a imagem em 0 a numero maximo de vendas +200
 
 plt.xlabel('Meses') # ajusta o label do eixo x para 'Meses'
 
 plt.ylabel('Vendas') # ajusta o label do eixo y para 'Vendas'
+
+plt.show()
+
+
+
+
+# Dispersão
+
+plt.scatter(meses,vendas)
+plt.show()
+
+
+
+
+# Barras
+
+plt.bar(meses,vendas)
+plt.show()
+
+
+
+
+# - Trabalhando com Múltiplos Gráficos no mesmo "Plot" -> Para melhor visualização/comparação
+
+# define que vamos configurar a primeira figura
+# figsize é opcional e define o tamanho da saida que sera impressa
+plt.figure(1,figsize=(15,8))
+
+# vamos informar quantas linhas vao ter na figura, quantas colunas e qual index dela (posicao no rol de figuras), respectivamente de acordo com os argumentos passados (podemos tambem passar sem a virgula (,) como nesse caso plt.subplot(131))
+plt.subplot(1,3,1) # ou plt.subplot(131)
+
+plt.plot(meses,vendas,'o',color='purple') # define meses no eixo x e vendas no eixo y, o parametro 'ro' define o gradico como marcadores conforme explicado acima, todos os tipos de marcadores encontram-se na documentacao do plt.plot()
+
+plt.axis([0,50,0,max(vendas)+200]) # define o dominio de x entre 0 e 50 e a imagem em 0 a numero maximo de vendas +200
+
+plt.xlabel('Meses') # ajusta o label do eixo x para 'Meses'
+
+plt.ylabel('Vendas') # ajusta o label do eixo y para 'Vendas'
+
+
+
+
+
+plt.subplot(1,3,2)
+
+plt.scatter(meses,vendas)
+
+
+
+
+
+plt.subplot(1,3,3)
+
+plt.bar(meses,vendas)
+
+
+
+
 
 plt.show()
