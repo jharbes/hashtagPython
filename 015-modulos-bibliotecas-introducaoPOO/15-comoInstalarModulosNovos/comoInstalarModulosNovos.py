419f7f4d2545bf18935739e08230ded29b6c0730
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-11 16:38:28 -0300

    modulos 15 commit 1

---
+++ 015-modulos-bibliotecas-introducaoPOO/15-comoInstalarModulosNovos/comoInstalarModulosNovos.py
@@ -0,0 +1,20 @@
+"""
+# Como instalar um Módulo/Biblioteca que não tenha no seu computador
+
+### Estrutura:
+
+- Usaremos o "pip" para isso. É um gerenciador de módulos/pacotes/bibliotecas que ajuda a gente a instalar, atualizar, desinstalar novos módulos
+- Não precisa dominar pip, apenas os comandos que vamos usar aqui mesmo
+
+Passo 1: Abrir o Prompt do Anaconda<br>
+Passo 2: Instalar com o pip os pacotes que quiser<br>
+Passo 3: Reiniciar o Jupyter (nem sempre necessário, mas na dúvida é melhor fazer)<br>
+
+"""
+import keyboard
+
+keyboard.press_and_release('ctrl+t')
+
+keyboard.write('hashtagtreinamentos.com')
+
+keyboard.press_and_release('enter')
