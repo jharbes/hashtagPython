451e96c33ec2ab6a5f3205c3d36ff583c4924625
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-18 08:33:37 -0300

    list comprehension 01 commit 1

---
+++ 017-listComprehension/01-listComprehension-oQueEh/listComprehension-oQueEh.py
@@ -0,0 +1,28 @@
+"""
+# List Comprehension - O que é e qual a importância?
+
+### Descrição:
+
+- List Comprehension é uma forma de iterar pelos elementas das listas de maneira "mais direta", com mais "cara de Python"
+- Em resumo: é como se você fizesse um "for" em 1 linha de código
+
+### Observação Importante:
+
+- Você não precisa de List comprehension para programar, tudo que vamos mostrar aqui dá pra fazer do jeito que já aprendemos
+- Você não vai sair de uma hora pra outra fazendo tudo list comprehension ao invés de for, porque é realmente mais confuso.
+- O objetivo aqui é:
+    1. Saber ler e entender o que tá acontecendo quando ver list comprehension (principal)
+    2. A medida do tempo você vai se acostumando com isso, vendo mais, usando mais e vai fazer naturalmente quando precisar.
+    
+- Mas se você sair desse módulo do curso achando isso tudo muito difícil, fica tranquilo, não usa por hora list comprehension e a medida que você for pegando mais experiência com o Python você lembra que tem esse módulo aqui e pode voltar no futuro
+
+### Estrutura:
+
+lista = [expressão for item in iterable]
+
+"""
+preco_produtos = [100, 150, 300, 5500]
+produtos = ['vinho', 'cafeiteira', 'microondas', 'iphone']
+
+# digamos que o imposto sobre os produtos é de 30%, ou seja, 0.3. Como eu faria para criar uma lista com os 
+# valores de imposto de cada produto?
