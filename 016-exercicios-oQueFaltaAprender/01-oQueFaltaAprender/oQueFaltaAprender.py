460de13aea61e55820d5abcb6254ab1f5787b0fa
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-18 08:25:20 -0300

    Create oQueFaltaAprender.py

---
+++ 016-exercicios-oQueFaltaAprender/01-oQueFaltaAprender/oQueFaltaAprender.py
@@ -0,0 +1,25 @@
+"""
+# O que falta aprender em Python?
+
+### Agora você já sabe Python
+
+### Só falta aprender:
+
+- Orientação a Objeto -> Classes, Métodos, etc.
+
+- "Jeitos mais avançados de fazer coisas que já sabemos": List Comprehensions, Lambda Expressions e Estrutura With
+
+- Aprender e se Acostumar com mais Módulos
+
+
+### O que você deve fazer agora?
+
+1. Nesse ponto, você já deve estar tranquilo de trabalhar com todas as estruturas e o que aprendemos e treinamos nos módulos anteriores.
+
+2. A partir de agora, vai de cada um, mas minha recomendação é: siga o ritmo do curso normalmente. Fora isso:
+    - Se você quer dominar tudo de Python, é só seguir o ritmo normal do Programa e fazer os módulos que faltam, antes de entrar nas Aplicações de Mercado
+    - Se você tá com pressa e quer ir direto pra Data Science ou para Automações, recomendo pelo menos assistir a Seção de List Comprehension, Lambda Expressions e With porque isso pode ser usado em basicamente qualquer código e é bom pelo menos você saber ler esses códigos.
+    - Se você quer ir para aplicações de Desenvolvedor, construir sites e aplicativos, então siga o ritmo normal do Curso porque a parte de Classes e Orientação a Objeto é muito importante para você
+    - No limite, você poderia pular os módulos que faltam do Programa e ir direto para as Aplicações do Mercado de Trabalho, que em muitos casos você vai se sair bem, mas saiba que vai ter "gaps" e "falhas" no seu conhecimento de Python que podem acabar impactando e dificultando os seus programas sem você saber. Então eu só recomendo você pular alguma coisa se estiver com pressa para ir para alguma aplicação específica logo
+    
+"""
