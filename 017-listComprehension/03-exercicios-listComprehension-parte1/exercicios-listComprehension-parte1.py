2336b9e5743bc5520cc5f92471cf8e1b1f927d63
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-18 09:45:30 -0300

    list comprehension 03 fim

--- 017-listComprehension/03-exercicios-listComprehension-parte1/exercicios-listComprehension-parte1.py
+++ 017-listComprehension/03-exercicios-listComprehension-parte1/exercicios-listComprehension-parte1.py
@@ -18,7 +18,12 @@ print(vendas_2019) # [558147, 712350, 573823, 405252, 718654, 531580, 973139, 89
 
 
 
+
 # - Agora, qual o maior valor de vendas de 2019?
+print(max(vendas_2019)) # 973139
+
+# ou
+
 vendas_2019.sort(reverse=True)
 
 print(vendas_2019) # [973139, 892292, 887061, 718654, 712350, 591120, 573823, 558147, 531580, 489705, 438508, 422760, 405252, 328311, 237467, 154753]
@@ -31,6 +36,10 @@ print(vendas_2019[0]) # 973139
 # Temos 2 formas de fazer, refazendo o list comprehension ou consultando a lista original
 
 vendas_produtos_2019=[(vendas_2019,produto) for produto,vendas_2019,vendas_2020 in vendas_produtos]
+
+print(max(vendas_produtos_2019)[1]) # geladeira
+
+# ou
 
 print(vendas_produtos_2019) # [(558147, 'iphone'), (712350, 'galaxy'), (573823, 'ipad'), (405252, 'tv'), (718654, 'máquina de café'), (531580, 'kindle'), (973139, 'geladeira'), (892292, 'adega'), (422760, 'notebook dell'), (154753, 'notebook hp'), (887061, 'notebook asus'), (438508, 'microsoft surface'), (237467, 'webcam'), (489705, 'caixa de som'), (328311, 'microfone'), (591120, 'câmera canon')]
 
