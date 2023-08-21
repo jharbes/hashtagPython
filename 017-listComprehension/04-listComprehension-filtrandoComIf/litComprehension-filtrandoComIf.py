c9341a001c4627588682f442e9f7f3e3d604e443
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-21 08:23:28 -0300

    list comprehension 04 commit 1

---
+++ 017-listComprehension/04-listComprehension-filtrandoComIf/litComprehension-filtrandoComIf.py
@@ -0,0 +1,13 @@
+"""
+# List Comprehensions com if para "filtrar" itens
+
+### Estrutura:
+
+lista = [expressão for item in iterable if condicao]
+
+"""
+# - Digamos que eu queira criar uma lista de produtos que bateram a meta.
+
+meta = 1000
+vendas_produtos = [1500, 150, 2100, 1950]
+produtos = ['vinho', 'cafeiteira', 'microondas', 'iphone']
