47175222420a00c8378758921c6541712c7c38e8
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-21 08:28:18 -0300

    list comprehension 04 commit 2

--- 017-listComprehension/04-listComprehension-filtrandoComIf/litComprehension-filtrandoComIf.py
+++ 017-listComprehension/04-listComprehension-filtrandoComIf/litComprehension-filtrandoComIf.py
@@ -11,3 +11,24 @@ lista = [expressão for item in iterable if condicao]
 meta = 1000
 vendas_produtos = [1500, 150, 2100, 1950]
 produtos = ['vinho', 'cafeiteira', 'microondas', 'iphone']
+
+
+
+
+# Fazendo por For tradicional
+
+vendas_produtos_meta_batida=[]
+for i,produto in enumerate(produtos):
+    if vendas_produtos[i]>=1000:
+        vendas_produtos_meta_batida.append(produto)
+
+print(vendas_produtos_meta_batida) # ['vinho', 'microondas', 'iphone']
+
+
+
+
+# Fazendo por list comprehension
+
+vendas_produtos_meta_batida2=[produto for i,produto in enumerate(produtos) if vendas_produtos[i]>=1000]
+
+print(vendas_produtos_meta_batida2) # ['vinho', 'microondas', 'iphone']
