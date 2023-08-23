9c47b321b793de95766e7c1d5714fb01fba7b6c7
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-23 16:28:30 -0300

    Create listComprehension-naoApenasParaListas.py

---
+++ 017-listComprehension/08-listComprehension-naoApenasParaListas/listComprehension-naoApenasParaListas.py
@@ -0,0 +1,15 @@
+"""
+# List Comprehension não serve só para criar uma lista, serve para qualquer ação em iterable
+
+### Exemplo:
+
+"""
+# - Vamos calcular quantos % das vendas o meu top 5 produtos representa das vendas totais
+
+produtos = ['coca', 'pepsi', 'guarana', 'skol', 'brahma', 'agua', 'del valle', 'dolly', 'red bull', 'cachaça', 'vinho tinto', 'vodka', 'vinho branco', 'tequila', 'champagne', 'gin', 'guaracamp', 'matte', 'leite de castanha', 'leite', 'jurupinga', 'sprite', 'fanta']
+vendas = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450, 800]
+top5 = ['agua', 'brahma', 'skol', 'coca', 'leite de castanha']
+
+
+# - Fazendo por for
+
