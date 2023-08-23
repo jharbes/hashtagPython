0f0d59b1969580d6ecd756782efc6613488ae36a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-23 16:36:44 -0300

    list comprehension 08 commit 2

--- 017-listComprehension/08-listComprehension-naoApenasParaListas/listComprehension-naoApenasParaListas.py
+++ 017-listComprehension/08-listComprehension-naoApenasParaListas/listComprehension-naoApenasParaListas.py
@@ -13,3 +13,22 @@ top5 = ['agua', 'brahma', 'skol', 'coca', 'leite de castanha']
 
 # - Fazendo por for
 
+total_top5=0
+for i,produto in enumerate(produtos):
+    if produto in top5:
+        total_top5+=vendas[i]
+
+
+print(total_top5) # 8461
+
+print('Top 5 representou {:0.1%} das vendas'.format(total_top5/sum(vendas))) # Top 5 representou 50.6% das vendas
+
+
+
+
+# - Fazendo por list comprehension
+
+total_top5=sum([vendas[i] for i,produto in enumerate(produtos) if produto in top5])
+
+print(total_top5)
+print('Top 5 representou {:0.1%} das vendas'.format(total_top5/sum(vendas)))
