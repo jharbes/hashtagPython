3594c0a2adf0bbf00eb8b40f03d164b87f7fea37
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-24 15:46:13 -0300

    sort commit 1

---
+++ 019-funcoesComoParametros-expressoesLambda/02-aplicandoFunctionEmIterable-sort/aplicandoFunctionsEmIterables-sort.py
@@ -0,0 +1,14 @@
+"""
+# sort (ou sorted) com function
+
+### Descrição:
+
+Até agora no programa, usamos várias vezes o .sort() para ordenar listas
+
+Mas o método sort tem um parâmetro que nunca usamos e que agora sabemos usar.
+
+"""
+produtos = ['apple tv', 'mac', 'IPhone x', 'IPhone 11', 'IPad', 'apple watch', 'mac book', 'airpods']
+produtos.sort()
+
+print(produtos)
