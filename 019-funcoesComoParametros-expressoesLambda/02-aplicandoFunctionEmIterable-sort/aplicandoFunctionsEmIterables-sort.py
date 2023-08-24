370fd24cd2eef13f20ecf943793dc485e8a01edc
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-24 16:10:26 -0300

    functions como parametros 02 commit final

--- 019-funcoesComoParametros-expressoesLambda/02-aplicandoFunctionEmIterable-sort/aplicandoFunctionsEmIterables-sort.py
+++ 019-funcoesComoParametros-expressoesLambda/02-aplicandoFunctionEmIterable-sort/aplicandoFunctionsEmIterables-sort.py
@@ -28,3 +28,26 @@ produtos2=['apple tv', 'mac', 'IPhone x', 'IPhone 11', 'IPad', 'apple watch', 'm
 produtos2.sort(key=str.lower)
 
 print(produtos2) # ['airpods', 'apple tv', 'apple watch', 'IPad', 'IPhone 11', 'IPhone x', 'mac', 'mac book']
+
+
+
+
+### Outro exemplo: como ordenar um dicionário de acordo com o valor
+
+vendas_produtos = {'vinho': 100, 'cafeiteira': 150, 'microondas': 300, 'iphone': 5500}
+
+vendas_produtos_list=[(vendas_produtos[produto],produto) for produto in vendas_produtos]
+
+vendas_produtos_list.sort(reverse=True)
+
+print(vendas_produtos_list) # [(5500, 'iphone'), (300, 'microondas'), (150, 'cafeiteira'), (100, 'vinho')]
+
+lista_vendas=[(quantidade,produto) for produto,quantidade in vendas_produtos_list]
+
+print(lista_vendas) # [('iphone', 5500), ('microondas', 300), ('cafeiteira', 150), ('vinho', 100)]
+
+
+
+# - Queremos listar da maior quantidade de vendas para a menor, para enviar como report para o diretor, por exemplo
+
+print(dict(lista_vendas)) # {'iphone': 5500, 'microondas': 300, 'cafeiteira': 150, 'vinho': 100}
