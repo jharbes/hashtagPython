4685f93a641e3810e0f08097bf9dfe19f2e09676
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-24 16:13:08 -0300

    03 lambda expressions commit 1

---
+++ 019-funcoesComoParametros-expressoesLambda/03-lambdaExpressions/lambdaExpressions.py
@@ -0,0 +1,21 @@
+"""
+# Lambda Expressions
+
+### Objetivo:
+
+- As lambdas expressions são funções anônimas (sem nome mesmo) que tem 1 linha de código e são atribuidas a uma variável, como se a variável virasse uma função.
+- Elas normalmente são usadas para fazer uma única ação, mas em Python usamos principalmente dentro de métodos como argumento, para não precisarmos criar uma função só para isso (vamos ver isso na aula que vem)
+- Outra aplicação delas está em criar um "gerador de funções" (vamos ver na 3ª Aula dessa Seção)
+
+### Obs
+
+- Não é "obrigatório" usar lambda expression, até porque praticamente tudo o que você faz com elas você consegue fazer com functions normais. Mas, é importante saber entender quando encontrar e saber usar a medida que você for se acostumando e vendo necessidade
+
+### Estrutura:
+
+minha_funcao = lambda parametro: expressão
+
+"""
+
+# - Exemplo mais simples
+
