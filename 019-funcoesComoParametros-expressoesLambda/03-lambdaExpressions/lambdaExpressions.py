7db18c169880ac3227dcf6a56332e29d4f39bb3d
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-24 16:23:41 -0300

    03 lambda expressions commit final

--- 019-funcoesComoParametros-expressoesLambda/03-lambdaExpressions/lambdaExpressions.py
+++ 019-funcoesComoParametros-expressoesLambda/03-lambdaExpressions/lambdaExpressions.py
@@ -19,3 +19,23 @@ minha_funcao = lambda parametro: expressão
 
 # - Exemplo mais simples
 
+def minha_funcao(num):
+    return num*2
+
+print(minha_funcao(5)) # 10
+
+
+funcao_lambda=lambda x:x*2
+
+print(funcao_lambda(5)) # 10
+
+
+
+
+# - Exemplo útil: Vamos usar lambda expressions para criar uma função que calcula o preço dos produtos acrescido do imposto
+
+imposto = 0.3
+
+minha_funcao2=lambda preco:preco+preco*imposto
+
+print(minha_funcao2(10)) # 13.0
