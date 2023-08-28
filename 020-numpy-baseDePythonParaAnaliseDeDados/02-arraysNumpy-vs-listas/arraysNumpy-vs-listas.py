b5d450dd0a71e2aa3556a550b748ac0efd0f7825
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-28 13:29:11 -0300

    numpy 01 commit 1

---
+++ 020-numpy-baseDePythonParaAnaliseDeDados/02-arraysNumpy-vs-listas/arraysNumpy-vs-listas.py
@@ -0,0 +1,22 @@
+"""
+# Guia de Introdução ao NumPy
+
+NumPy, que significa Numerical Python, é uma biblioteca fundamental para a computação científica em Python. Mas suas capacidades vão muito além disso, como veremos nos nestes vídeos. E ele serve como uma boa base para o Pandas, que é uma das bibliotecas mais populares para análise de dados em Python.
+
+## Instalação
+
+Para instalar o NumPy, você pode usar o comando `pip install numpy` no terminal.
+
+# pip install numpy
+
+
+## Arrays NumPy
+
+Um array é uma estrutura de dados que armazena valores de um mesmo tipo. Em Python, isso é uma grande vantagem porque economiza espaço e permite operações mais eficientes.
+
+"""
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+
+print(array) # [1 2 3 4 5]
