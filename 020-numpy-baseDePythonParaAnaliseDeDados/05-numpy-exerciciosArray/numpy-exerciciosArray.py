ffd92e409b51b8e7c507ae189e7bd0cb6d96e536
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-18 08:12:59 -0300

    alteracao nome diretorio

---
+++ 020-numpy-baseDePythonParaAnaliseDeDados/05-numpy-exerciciosArray/numpy-exerciciosArray.py
@@ -0,0 +1,9 @@
+"""
+## Exercício 1
+
+Você é um gerente de vendas e tem os dados de vendas de um produto para os últimos 7 dias em uma lista: `[127, 90, 201, 150, 210, 220, 115]`. Calcule a média de vendas durante a semana.
+
+"""
+import numpy as np
+
+dados = [127, 90, 201, 150, 210, 220, 115]
