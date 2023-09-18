86fbcd1863fc0bfac471b7910ef50a3996bb70a8
Jorge Harbes <jharbes@hotmail.com>
2023-09-18 18:23:41 -0300

    Update numpy-where.py

--- 020-numpy-baseDePythonParaAnaliseDeDados/07-numpy-where/numpy-where.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/07-numpy-where/numpy-where.py
@@ -28,9 +28,45 @@ funcionarios_acima_media = np.where(salarios > media_salarial)
 print(funcionarios_acima_media) # (array([2, 4, 5, 6], dtype=int64),)
 
 
-
 print(salarios[funcionarios_acima_media]) # [4000 4500 4000 5000]
 
 
-
 print(salarios[salarios > media_salarial]) # [4000 4500 4000 5000]
+
+
+print(salarios[salarios >= 4000]) # [4000 4500 4000 5000]
+
+
+print(np.where(salarios > media_salarial, 'acima da média', 'abaixo da média')) # ['abaixo da média' 'abaixo da média' 'acima da média' 'abaixo da média' 'acima da média' 'acima da média' 'acima da média']
+
+
+
+# dar bônus de 10% para os funcionários com salários abaixo da média
+salarios_bonus = np.where(salarios < media_salarial, salarios * 1.1, salarios)
+
+print(salarios) # [3000 3500 4000 2000 4500 4000 5000]
+
+print(salarios_bonus) # [3300. 3850. 4000. 2200. 4500. 4000. 5000.]
+
+
+
+# considerando novamente nossos salários originais
+print(salarios) # [3000 3500 4000 2000 4500 4000 5000]
+
+
+# filtrar os salários entre 3000 e 4500 com where
+print(np.where((salarios >= 3000) & (salarios <= 4500))) # (array([0, 1, 2, 4, 5], dtype=int64),)
+
+
+
+salarios_ajustados = np.where((salarios >= 3000) & (salarios <= 4500), salarios * 1.05, salarios)
+print(salarios_ajustados) # [3150. 3675. 4200. 2000. 4725. 4200. 5000.]
+
+
+# filtrar os salários abaixo de 3000 ou acima de 4500 com where
+print(np.where((salarios_ajustados < 3000) | (salarios_ajustados > 4500))) # (array([3, 4, 6], dtype=int64),)
+
+
+
+salarios_ajustados = np.where((salarios_ajustados < 3000) | (salarios_ajustados > 4500), salarios_ajustados * 1.1, salarios_ajustados)
+print(salarios_ajustados) # [3150.  3675.  4200.  2200.  5197.5 4200.  5500. ]
