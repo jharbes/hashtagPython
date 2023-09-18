ffd7b816b1b7a38915fbf705582b143d06db1cb9
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-18 13:22:12 -0300

    numpy 06 commit 3

--- 020-numpy-baseDePythonParaAnaliseDeDados/06-aleatorios-estatistica/numpy-aleatorios-estatistica.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/06-aleatorios-estatistica/numpy-aleatorios-estatistica.py
@@ -4,7 +4,24 @@
 NumPy, que significa Numerical Python, é uma biblioteca fundamental para a computação científica em Python. Ela fornece suporte para arrays e matrizes, além de funções matemáticas para operações com esses objetos. É, também, a base da biblioteca Pandas.
 
 """
+"""
+Breve resumo e conceitos simplificados das funções estatísticas citadas:
 
+1. Mediana:
+A mediana é um valor que divide um conjunto de dados em duas partes iguais. Para encontrá-la, você deve organizar os dados em ordem crescente ou decrescente e escolher o valor do meio. Se houver um número ímpar de dados, a mediana será exatamente o valor central. Se houver um número par de dados, a mediana será a média dos dois valores do meio.
+
+2. Percentil:
+O percentil é uma medida estatística que indica a posição relativa de um dado dentro de um conjunto de dados. Ele informa a porcentagem de valores que estão abaixo desse dado. Por exemplo, o percentil 50 (também conhecido como mediana) divide os dados em duas partes iguais, com 50% dos valores abaixo dele e 50% acima.
+
+3. Desvio padrão:
+O desvio padrão é uma medida que indica o quão dispersos os valores de um conjunto de dados estão em relação à média. Ele mostra a variabilidade dos dados em relação ao valor médio. Um desvio padrão maior indica que os dados estão mais espalhados, enquanto um desvio padrão menor indica que os dados estão mais próximos da média.
+
+4. Variância:
+A variância é outra medida de dispersão que indica o quão distantes os valores de um conjunto de dados estão da média. Ela é calculada como a média dos quadrados das diferenças entre cada valor e a média. A variância fornece uma medida da dispersão total dos dados, independentemente de serem maiores ou menores que a média.
+
+Essas medidas são amplamente utilizadas na estatística para resumir e analisar conjuntos de dados. Elas fornecem informações valiosas sobre a distribuição, a variabilidade e a posição dos dados, permitindo uma compreensão mais completa dos mesmos.
+
+"""
 ## Números aleatórios e estatística básica  
 
 import numpy as np
@@ -70,3 +87,9 @@ print(np.median(dados_vendas)) # 129.5
 
 
 print(np.percentile(dados_vendas, 50)) # 129.5
+
+
+print(np.std(dados_vendas)) # 39.305300745149715
+
+
+print(np.var(dados_vendas)) # 1544.9066666666665
