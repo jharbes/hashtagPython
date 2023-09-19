f80e162dc8a225f2003b10a11b0e884d969b8bf4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 13:11:11 -0300

    numpy 08 commit final

--- 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
@@ -57,3 +57,63 @@ lista2=[2<5,not True,5!=5,6==6]
 
 # observe que a soma de True sempre será 1 e False sempre será zero (True == 1) (False == 0)
 print(sum(lista2)) # 2
+
+
+
+
+### Bônus
+
+# Outras formas de fazer contagem
+# IMPORTANTE***: Lembrar que salarios > media_salario é um array do Numpy
+
+print(salarios>media_salarios) # [False False  True  True False  True False  True  True]
+
+
+# A funcao np.count_nonzero() conta o numero de elementos que seja DIFERENTES do valor zero
+print(np.count_nonzero(salarios > media_salarios)) # 5
+
+
+# A funcao np.unique() retorna um array apenas com os valores ÚNICOS, ou seja, não repetidos
+print(np.unique(salarios > media_salarios)) # [False  True]
+
+
+# Agora com o argumento 'return_counts' ele retorna um tupla onde o primeiro elemento é o array numpy em si COM OS ELEMENTOS UNICOS e o segundo elemento será a contagem de cada um dos elementos quantas vezes se repete no array original, na mesma ordem que os elementos se encontram no aray resultante
+print(np.unique(salarios > media_salarios, return_counts=True))
+
+# fazendo o unpacking da tupla:
+valores_unicos, contagem = np.unique(salarios > media_salarios, return_counts=True)
+
+print(valores_unicos) # [False  True]
+print(contagem) # [4 5]
+
+
+
+
+### Bônus 2
+
+# Criando 20 valores fictícios de salários com a mesma faixa anterior, tomando cuidado de outras pessoas gerarem os mesmos valores:
+
+rng = np.random.default_rng(seed=42)
+
+salarios_gerados = rng.integers(low=np.min(salarios), high=np.max(salarios), size=20)
+
+print(salarios_gerados) # [2249 4167 3832 3228 3212 4404 2240 3952 2564 2263 3474 4731 4060 4131 4008 4200 3437 2358 4351 3261]
+
+
+
+
+rng = np.random.default_rng(seed=42)
+
+# agora estamos fazendo exatamente como o exemplo anterior, porem com o argumento 'endpoint=True' o que significa que o valor setado para o argumento 'high' será incluido entre os numeros aleatorios gerados, porque por default o valor de 'high' fica de fora do conjunto de numeros aptos a aleatoriedade
+salarios_gerados = rng.integers(low=np.min(salarios), high=np.max(salarios), size=20, endpoint=True)
+
+print(salarios_gerados) # [2249 4167 3833 3229 3212 4404 2240 3953 2564 2263 3474 4732 4060 4131 4009 4201 3437 2358 4352 3261]
+
+
+print(np.mean(salarios_gerados)) # 3506.45
+
+
+# as funções estatísticas também podem ser chamadas como métodos dos arrays, observe que a SINTAXE ESTÁ DIFERENTE
+print(salarios_gerados.min()) # 2240
+print(salarios_gerados.max()) # 4732
+print(salarios_gerados.mean()) # 3506.45
