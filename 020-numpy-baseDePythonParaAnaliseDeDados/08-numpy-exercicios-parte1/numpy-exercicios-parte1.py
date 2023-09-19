631546644f3579230532e8b02e81ac99fbbd59ff
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 11:48:44 -0300

    Create numpy-exercicios-parte1.py

---
+++ 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
@@ -0,0 +1,9 @@
+"""
+## Exercício
+
+Você é um analista de RH e tem os salários de todos os funcionários da sua empresa em um array NumPy. Seu trabalho é identificar quantos funcionários ganham acima da média. Use o seguinte array para sua análise: `salarios = np.array([3000, 2500, 3500, 4000, 2000, 4500, 3000, 3800, 4800])`.
+
+"""
+import numpy as np
+
+salarios = np.array([3000, 2500, 3500, 4000, 2000, 4500, 3000, 3800, 4800])
