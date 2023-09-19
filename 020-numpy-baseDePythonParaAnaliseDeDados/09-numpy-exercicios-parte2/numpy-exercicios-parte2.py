29464f0e74dcfad36867090dc1e5c7a4456e42b4
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 14:41:13 -0300

    numpy 09 commit bonus 1

--- 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-exercicios-parte2.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-exercicios-parte2.py
@@ -22,3 +22,23 @@ print(dois_desvio_padrao) # 1.185092588975412
 tempos_fora=tempos_ciclo[(tempos_ciclo<media-dois_desvio_padrao) | (tempos_ciclo>media+dois_desvio_padrao)]
 
 print(tempos_fora) # [7.2]
+
+
+
+
+### Bônus
+
+condicao = ((tempos_ciclo > media + 2 * desvio_padrao) | (tempos_ciclo < media - 2 * desvio_padrao))
+
+print(condicao) # [False False False False False False False  True False]
+
+print(np.where(condicao)) # [False False False False False False False  True False]
+
+
+contrario_condicao = ((tempos_ciclo <= media + 2 * desvio_padrao) & (tempos_ciclo >= media - 2 * desvio_padrao))
+
+print(np.where(contrario_condicao)) # (array([0, 1, 2, 3, 4, 5, 6, 8], dtype=int64),)
+
+
+# observe aqui que a resposta é identico ao exemplo anterior que é o inversao da condicao principal de todo o exercicio, o que queremos mostrar é que podemos negar uma condicao e conseguir o inverso dela apenas utilizando o caractere til (~) na frente dela
+print(np.where(~condicao)) # (array([0, 1, 2, 3, 4, 5, 6, 8], dtype=int64),)
