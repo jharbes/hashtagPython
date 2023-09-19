472d03a7fdf1b655d23a02e6b8eb4ba7d2108f9e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 12:20:32 -0300

    numpy 08 commit 3

--- 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
@@ -19,11 +19,20 @@ print(type(acima_media)) # <class 'tuple'>
 print(acima_media) # (array([2, 3, 5, 7, 8], dtype=int64),)
 
 
+salarios_acima_media=salarios[salarios>media_salarios]
+print(salarios_acima_media) # [3500 4000 4500 3800 4800]
+print(type(salarios_acima_media)) # <class 'numpy.ndarray'>
 
-# Faremos de duas maneiras:
+
+
+# Faremos de três maneiras:
 
 # length da lista salarios[acima_media]
 print(len(salarios[acima_media])) # 5
 
 # size do primeiro elemento da tupla resultante do where acima
 print(acima_media[0].size) # 5
+
+# somando as condicoes True do array
+print(salarios>media_salarios) # [False False  True  True False  True False  True  True]
+print(np.sum(salarios>media_salarios)) # 5
