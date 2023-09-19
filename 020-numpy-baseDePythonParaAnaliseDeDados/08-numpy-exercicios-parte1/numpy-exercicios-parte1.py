6f3a137ed80560ca9c3eae24ee76d21550805036
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 12:07:39 -0300

    numpy 08 commit 2

--- 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/08-numpy-exercicios-parte1/numpy-exercicios-parte1.py
@@ -7,3 +7,23 @@ Você é um analista de RH e tem os salários de todos os funcionários da sua e
 import numpy as np
 
 salarios = np.array([3000, 2500, 3500, 4000, 2000, 4500, 3000, 3800, 4800])
+
+
+media_salarios=np.mean(salarios)
+
+print(media_salarios) # 3455.5555555555557
+
+acima_media=np.where(salarios>media_salarios)
+
+print(type(acima_media)) # <class 'tuple'>
+print(acima_media) # (array([2, 3, 5, 7, 8], dtype=int64),)
+
+
+
+# Faremos de duas maneiras:
+
+# length da lista salarios[acima_media]
+print(len(salarios[acima_media])) # 5
+
+# size do primeiro elemento da tupla resultante do where acima
+print(acima_media[0].size) # 5
