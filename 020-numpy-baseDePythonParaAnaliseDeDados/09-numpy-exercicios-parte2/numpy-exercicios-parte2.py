a9d1b1dc4aa1261da8bebb1aa3fa3c42fd867568
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 14:54:01 -0300

    numpy 09 commit final

--- 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-exercicios-parte2.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-exercicios-parte2.py
@@ -42,3 +42,40 @@ print(np.where(contrario_condicao)) # (array([0, 1, 2, 3, 4, 5, 6, 8], dtype=int
 
 # observe aqui que a resposta é identico ao exemplo anterior que é o inversao da condicao principal de todo o exercicio, o que queremos mostrar é que podemos negar uma condicao e conseguir o inverso dela apenas utilizando o caractere til (~) na frente dela
 print(np.where(~condicao)) # (array([0, 1, 2, 3, 4, 5, 6, 8], dtype=int64),)
+
+print(~condicao) # [ True  True  True  True  True  True  True False  True]
+
+
+## IMPORTANTE!!
+# Lembrar que no Python puro a negação é feita com not
+print(not True) # False
+
+
+
+
+### Bônus 2
+
+rng = np.random.default_rng(seed=0)
+
+# a opção de 'normal' utilizada abaixo faz com que os números gerados respeitem uma distribuição normal, onde o 'loc' será o valor da media, o 'scale' será o desvio padrão e o 'size' será o numero de elementos
+tempos_gerados = rng.normal(loc=media, scale=desvio_padrao, size=100)
+
+print(tempos_gerados) # array de 100 numeros
+
+
+# observe que o valor da média não será EXATAMENTE IGUAL, será utilizado apenas como referência pois o algoritmo terá que acomodar todos os dados de maneira a respeitar dados referentes a uma distribuição normal
+print(media) # 5.8
+print(tempos_gerados.mean()) # 5.848053545223129
+
+
+# os valores nao sao identicos pelos mesmos motivos explicados acima
+print(desvio_padrao) # 0.592546294487706
+print(tempos_gerados.std()) # 0.5701068395534514
+
+
+# utilizando a mesma condicao do exercicio original
+condicao = ((tempos_gerados > tempos_gerados.mean() + 2 * tempos_gerados.std()) | (tempos_gerados < tempos_gerados.mean() - 2 * tempos_gerados.std()))
+
+print(np.where(condicao)) # (array([12, 69], dtype=int64),)
+
+print(tempos_gerados[condicao]) # [4.42231163 4.49431839]
