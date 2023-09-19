5b6fd34bc05379467a9debb41af8bc7ee0640720
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-19 14:17:19 -0300

    numpy 09 commit 3

--- 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-execicios-parte2.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/09-numpy-exercicios-parte2/numpy-execicios-parte2.py
@@ -8,3 +8,17 @@ import numpy as np
 
 tempos_ciclo = np.array([5.5, 5.7, 5.9, 6.0, 5.8, 5.6, 5.7, 7.2, 4.8])
 media = np.mean(tempos_ciclo) 
+
+print(media) # 5.8
+
+
+desvio_padrao=np.std(tempos_ciclo)
+print(desvio_padrao) # 0.592546294487706
+
+dois_desvio_padrao=2*desvio_padrao
+print(dois_desvio_padrao) # 1.185092588975412
+
+
+tempos_fora=tempos_ciclo[(tempos_ciclo<media-dois_desvio_padrao) | (tempos_ciclo>media+dois_desvio_padrao)]
+
+print(tempos_fora) # [7.2]
