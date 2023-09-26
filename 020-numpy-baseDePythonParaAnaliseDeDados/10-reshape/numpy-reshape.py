dc1b10be8bf7a483bf2b8bcec601491db7143b88
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-09-26 10:57:44 -0300

    Update numpy-reshape.py

--- 020-numpy-baseDePythonParaAnaliseDeDados/10-reshape/numpy-reshape.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/10-reshape/numpy-reshape.py
@@ -21,3 +21,34 @@ vendas_reshaped = np.reshape(vendas, (2, 7))
 print(vendas_reshaped) # [[200 220 250 210 300 280 230] [210 220 240 230 210 280 220]]
 
 
+print(vendas.ndim) # 1
+
+print(vendas.shape) # (14,)
+
+print(vendas_reshaped.ndim) # 2
+
+print(vendas_reshaped.shape) # (2, 7)
+
+print(vendas_reshaped) # [[200 220 250 210 300 280 230] [210 220 240 230 210 280 220]]
+
+print(vendas_reshaped[0]) # [200 220 250 210 300 280 230]
+
+print(vendas_reshaped[1]) # [210 220 240 230 210 280 220]
+
+print(vendas_reshaped[0][0]) # 200
+
+print(vendas_reshaped[1][0]) # 210
+
+print(vendas_reshaped.sum()) # 3300
+
+print(vendas_reshaped.shape) # (2, 7)
+
+print(vendas_reshaped) # [[200 220 250 210 300 280 230] [210 220 240 230 210 280 220]]
+
+
+# axis=0 colunas, axis=1 linhas
+print(vendas_reshaped.sum(axis=0)) # [410 440 490 440 510 560 450]
+
+print(vendas_reshaped.sum(axis=1)) # [1690 1610]
+
+print(vendas_reshaped.mean(axis=1)) # [241.42857143 230.        ]
