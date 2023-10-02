0c52afee757d3f157d5359b57ebda5d7a9b82b49
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-02 13:19:55 -0300

    Create numpy-exercicios-parte3.py

---
+++ 020-numpy-baseDePythonParaAnaliseDeDados/12-numpy-exercicios-parte3/numpy-exercicios-parte3.py
@@ -0,0 +1,21 @@
+"""
+## Exercício
+
+Você é um gerente de vendas e tem os dados de vendas de três produtos diferentes (Produto A, Produto B, Produto C) para os últimos 5 dias em um array 2D NumPy. Cada linha do array representa um produto e cada coluna representa um dia. Seu trabalho é calcular as vendas totais para cada produto e para cada dia.
+
+Use o seguinte array para sua análise:
+
+```python
+vendas = np.array([[50, 60, 70, 65, 80],
+                   [85, 90, 78, 92, 88],
+                   [72, 75, 68, 77, 76]])
+```
+
+**Solução:**
+
+"""
+import numpy as np
+
+vendas = np.array([[50, 60, 70, 65, 80],
+                   [85, 90, 78, 92, 88],
+                   [72, 75, 68, 77, 76]])
