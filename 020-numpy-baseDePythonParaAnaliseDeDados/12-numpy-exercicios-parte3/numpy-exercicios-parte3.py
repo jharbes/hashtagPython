4550b682bb36413cee1daf5bb2e2257a5cdc00fd
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-02 13:32:26 -0300

    numpy 12 exercicios minha solucao

--- 020-numpy-baseDePythonParaAnaliseDeDados/12-numpy-exercicios-parte3/numpy-exercicios-parte3.py
+++ 020-numpy-baseDePythonParaAnaliseDeDados/12-numpy-exercicios-parte3/numpy-exercicios-parte3.py
@@ -16,6 +16,26 @@ vendas = np.array([[50, 60, 70, 65, 80],
 """
 import numpy as np
 
+lista_produtos=np.array(['Produto A','Produto B','Produto C'])
+
 vendas = np.array([[50, 60, 70, 65, 80],
                    [85, 90, 78, 92, 88],
                    [72, 75, 68, 77, 76]])
+
+
+vendas_totais_produto=vendas.sum(axis=1)
+
+print(f'{vendas_totais_produto=}\n') # vendas_totais_produto=array([325, 433, 368]
+
+for i,venda in enumerate(vendas_totais_produto):
+    print('Número de vendas do {} foram {}'.format(lista_produtos[i],venda))
+
+
+
+
+vendas_totais_dia=vendas.sum(axis=0)
+
+print(f'{vendas_totais_dia=}\n') # vendas_totais_dia=array([207, 225, 216, 234, 244])
+
+for i,venda in enumerate(vendas_totais_dia):
+    print('Número de vendas do dia {} foram {}'.format(i+1,venda))
