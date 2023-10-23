66f1bd543fb980e6871ec6e83f88606861bd20ad
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-23 08:39:33 -0300

    Create 08-recuperar_uma_versao_antiga_e_atualizacoes_forcadas.py

---
+++ 048-github_para_organizacao_de_projetos/08-recuperar_uma_versao_antiga_e_atualizacoes_forcadas.py
@@ -0,0 +1,25 @@
+"""
+Algumas maneiras de voltar a um commit antigo:
+
+
+- No GitHub Desktop:
+
+Imediatamente embaixo do repositório escolhido clicar em 'History', depois ir
+no ultimo commit existente e clicar com o segundo botão do mouse e ir na opção
+'Revert changes in commit'
+OBS**: Só conseguirá fazer um commit de cada vez
+
+
+Podemos também ir no commit da versão desejada criar uma nova Branch, para isso 
+vamos até o commit desejado clicamos com o segundo botão do mouse e vamos na opção
+'Create branch from commit' e seguimos ultilizando essa branch, após feitas todas as
+alterações podemos fazer um 'merge' novamente com a origin fazendo com que assim a
+branch origin tenha os valores desejados do commit em questão
+
+
+
+- No GitBash:
+
+
+
+"""
