0f6b169b4119f45b08f3626bd9189369d73d2832
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-23 08:56:51 -0300

    Update 08-recuperar_uma_versao_antiga_e_atualizacoes_forcadas.py

--- 048-github_para_organizacao_de_projetos/08-recuperar_uma_versao_antiga_e_atualizacoes_forcadas.py
+++ 048-github_para_organizacao_de_projetos/08-recuperar_uma_versao_antiga_e_atualizacoes_forcadas.py
@@ -20,6 +20,30 @@ branch origin tenha os valores desejados do commit em questão
 
 - No GitBash:
 
+Primeiramente teremos duas opçoes:
+1a - Retroceder o branch main ao commit desejado
+2a - Fazer um merge do branch desejado com o branch main levando ao estado desejado
+
+No terminal usaremos o seguinte comando:
+
+git reset --hard 342105d578af43734260005b788b652ef6438805 (codigo recuperado do commit desejado, podemos
+conseguir ele pela opção 'Copy SHA' clicando com o segundo botão do mouse no commit respectivo no próprio
+GitHub Desktop ou pelo GitHub achando o commit em questão e apertando na opção 'Copy the full SHA')
+
+ou
+
+git reset --hard <nome_da_branch_desejada_para_fazer_merge> (no caso de fazer um merge do main com a branch
+desejada)
+
+
+Após feitas as alterações será necessário fazer um 'push' dos arquivos atualizados do PC para o
+repositório levando as alterações desejadas para o GitHub (a principio só foram feitos na máquina local),
+para isso utilizaremos o comando:
+
+git push -f origin main
+
+
+IMPORTANTE***: O histórico antigo que foi voltado é perdido de maneira definitiva.
 
 
 """
